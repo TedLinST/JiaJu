a10bb49bf8430b3240c701ce8a93b79c6513fc65
zkey <774508556@qq.com>
2019-05-06 17:49:49 +0800

    新界面 - 设计师园地

--- src/app/project/pd/designers-garden/designers-garden.component.ts
+++ src/app/project/pd/designers-garden/designers-garden.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { PortalNavigation } from 'src/app/portal/portal.navigation';
 import { ConstantHandler } from 'src/modules/utils/constant-handler';
 import { DataDesignersGardenHandler } from 'src/data/designers-garden';
+import { KeyCarouselLabelEvent } from 'src/modules/key/carousel/carousel-label.event';
+import { Utils } from 'src/modules/utils/utils';
 
 @Component({
   selector: 'app-designers-garden',
@@ -24,6 +26,33 @@ export class DesignersGardenComponent implements OnInit {
   // 门户导航
   portalNav: any;
 
+  option1: any = false;
+  option2: any = true;
+  option3: any = '擅长风格 1';
+  option4: any = '';
+  option5: any = '';
+  option6: any = '';
+
+  labelArray: KeyCarouselLabelEvent[] = [
+    {
+      key: 'title',
+    },
+    {
+      label: '工作年龄',
+      key: 'number',
+    },
+    {
+      label: '头衔',
+      key: 'rank',
+    }
+  ];
+
+  pageSize = 4;
+
+  total = 0;
+
+  listData: any[] = [];
+
   constructor(private router: Router) {
     this.portalNav = new PortalNavigation(router);
   }
@@ -32,6 +61,8 @@ export class DesignersGardenComponent implements OnInit {
     this.CH_PD_BANNER_SRC = ConstantHandler.CH_PD_BANNER_SRC;
     this.CH_PD_BANNER_MENU = ConstantHandler.CH_PD_BANNER_MENU;
     this.dataHandler = DataDesignersGardenHandler;
+    this.total = this.dataHandler.DL.data.length;
+    this.handleListData(this.dataHandler.DL.data);
   }
 
   /**
@@ -54,6 +85,81 @@ export class DesignersGardenComponent implements OnInit {
         this.portalNav.openSoftwareIntegration();
       }
     }
+  }
+
+  /**
+   * 打开优秀设计师详情页
+   * @param id 
+   */
+  openDesignersGardenDetails(id: any) {
+    this.navigateDeveloping();
+  }
+
+  /**
+   * 导航到正在开发中页面
+   */
+  navigateDeveloping() {
+    this.router.navigateByUrl('developing');
+  }
+
+  /**
+   * 处理列表数据
+   */
+  handleListData(listData: any[]) {
+    if (listData != null && listData.length > 0) {
+      let listDataTemp = Utils.arrayRandomSort(listData);
+      this.listData = [];
+      let len = listData.length > this.pageSize ? this.pageSize : listData.length;
+      for (let i = 0; i < len; i++) {
+        this.listData.push(listDataTemp[i]);
+      }
+    } else {
+      this.listData = [];
+    }
+  }
+
+  /**
+   * 分页
+   */
+  onPageIndexChange() {
+    this.listData = Utils.arrayRandomSort(this.listData);
+  }
+
+  /**
+   * 设置下拉框选项
+   */
+  onSelectOption(key: any, value: any) {
+    this.listData = Utils.arrayRandomSort(this.listData);
+  }
+
+  /**
+   * 联系设计师
+   * @param id 
+   */
+  onTel(id: any) {
+    this.navigateDeveloping();
+  }
+
+  /**
+   * 发起合作
+   * @param id 
+   */
+  onCooperate(id: any) {
+    this.navigateDeveloping();
+  }
+
+  /**
+   * 家具论坛 - 更多
+   */
+  onClickMoreFF() {
+    this.navigateDeveloping();
+  }
+
+  /**
+   * 家具论坛详情
+   */
+  openFFDetails() {
+    this.navigateDeveloping();
   }
 
 }
--- src/data/designers-garden.ts
+++ src/data/designers-garden.ts
@@ -1,4 +1,406 @@
 // 设计师园地数据助手
 export const DataDesignersGardenHandler = {
+  // 优秀设计师
+  FD: {
+    title: '优秀设计师推荐',
+    enTitle: 'RECOMMENDED BY EXCELLENT DESIGNERS',
+    data: [
+      {
+        id: 1,
+        data: [
+          {
+            id: 1,
+            title: '设计师名称',
+            number: 1,
+            rank: 1,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          },
+          {
+            id: 2,
+            title: '设计师名称',
+            number: 2,
+            rank: 1,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          },
+          {
+            id: 3,
+            title: '设计师名称',
+            number: 3,
+            rank: 1,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          },
+          {
+            id: 4,
+            title: '设计师名称',
+            number: 4,
+            rank: 1,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          },
+          {
+            id: 5,
+            title: '设计师名称',
+            number: 5,
+            rank: 1,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          },
+          {
+            id: 6,
+            title: '设计师名称',
+            number: 6,
+            rank: 1,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          }
+        ]
+      },
+      {
+        id: 2,
+        data: [
+          {
+            id: 1,
+            title: '设计师名称',
+            number: 1,
+            rank: 2,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          },
+          {
+            id: 2,
+            title: '设计师名称',
+            number: 2,
+            rank: 2,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          },
+          {
+            id: 3,
+            title: '设计师名称',
+            number: 3,
+            rank: 2,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          },
+          {
+            id: 4,
+            title: '设计师名称',
+            number: 4,
+            rank: 2,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          },
+          {
+            id: 5,
+            title: '设计师名称',
+            number: 5,
+            rank: 2,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          },
+          {
+            id: 6,
+            title: '设计师名称',
+            number: 6,
+            rank: 2,
+            src: '/assets/imgs/designer-garden/shejishi.jpg'
+          }
+        ]
+      }
+    ]
+  },
+
+  // 设计师列表
+  DL: {
+    title: '设计师列表',
+    enTitle: 'DESIGNER LIST',
+    data: [
+      {
+        id: 1,
+        src: '/assets/imgs/designer-garden/shejishi.jpg',
+        name: '设计师1',
+        experience: '5年',
+        style: '传统中式',
+        place: '民用家具',
+        score: '9.0',
+        subData: [
+          {
+            id: 1,
+            title: '作品名称1',
+            number: 420,
+            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+          },
+          {
+            id: 2,
+            title: '作品名称2',
+            number: 2500,
+            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+          },
+          {
+            id: 3,
+            title: '作品名称3',
+            number: 100,
+            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+          }
+        ]
+      },
+      {
+        id: 2,
+        src: '/assets/imgs/designer-garden/shejishi.jpg',
+        name: '设计师2',
+        experience: '2年',
+        style: '传统中式',
+        place: '民用家具',
+        score: '9.0',
+        subData: [
+          {
+            id: 1,
+            title: '作品名称1',
+            number: 420,
+            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+          },
+          {
+            id: 2,
+            title: '作品名称2',
+            number: 2500,
+            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+          },
+          {
+            id: 3,
+            title: '作品名称3',
+            number: 100,
+            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+          }
+        ]
+      },
+      {
+        id: 3,
+        src: '/assets/imgs/designer-garden/shejishi.jpg',
+        name: '设计师3',
+        experience: '4年',
+        style: '传统中式',
+        place: '民用家具',
+        score: '9.0',
+        subData: [
+          {
+            id: 1,
+            title: '作品名称1',
+            number: 420,
+            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+          },
+          {
+            id: 2,
+            title: '作品名称2',
+            number: 2500,
+            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+          },
+          {
+            id: 3,
+            title: '作品名称3',
+            number: 100,
+            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+          }
+        ]
+      },
+      {
+        id: 4,
+        src: '/assets/imgs/designer-garden/shejishi.jpg',
+        name: '设计师4',
+        experience: '3年',
+        style: '传统中式',
+        place: '民用家具',
+        score: '9.0',
+        subData: [
+          {
+            id: 1,
+            title: '作品名称1',
+            number: 420,
+            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+          },
+          {
+            id: 2,
+            title: '作品名称2',
+            number: 2500,
+            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+          },
+          {
+            id: 3,
+            title: '作品名称3',
+            number: 100,
+            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+          }
+        ]
+      },
+      {
+        id: 5,
+        src: '/assets/imgs/designer-garden/shejishi.jpg',
+        name: '设计师5',
+        experience: '3年',
+        style: '传统中式',
+        place: '民用家具',
+        score: '9.0',
+        subData: [
+          {
+            id: 1,
+            title: '作品名称1',
+            number: 420,
+            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+          },
+          {
+            id: 2,
+            title: '作品名称2',
+            number: 2500,
+            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+          },
+          {
+            id: 3,
+            title: '作品名称3',
+            number: 100,
+            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+          }
+        ]
+      },
+      {
+        id: 6,
+        src: '/assets/imgs/designer-garden/shejishi.jpg',
+        name: '设计师6',
+        experience: '3年',
+        style: '传统中式',
+        place: '民用家具',
+        score: '9.0',
+        subData: [
+          {
+            id: 1,
+            title: '作品名称1',
+            number: 420,
+            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+          },
+          {
+            id: 3,
+            title: '作品名称3',
+            number: 100,
+            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+          },
+          {
+            id: 2,
+            title: '作品名称2',
+            number: 2500,
+            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+          }
+        ]
+      },
+      {
+        id: 7,
+        src: '/assets/imgs/designer-garden/shejishi.jpg',
+        name: '设计师7',
+        experience: '3年',
+        style: '传统中式',
+        place: '民用家具',
+        score: '9.0',
+        subData: [
+          {
+            id: 3,
+            title: '作品名称3',
+            number: 100,
+            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+          },
+          {
+            id: 1,
+            title: '作品名称1',
+            number: 420,
+            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+          },
+          {
+            id: 2,
+            title: '作品名称2',
+            number: 2500,
+            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+          }
+        ]
+      },
+      {
+        id: 8,
+        src: '/assets/imgs/designer-garden/shejishi.jpg',
+        name: '设计师8',
+        experience: '3年',
+        style: '传统中式',
+        place: '民用家具',
+        score: '9.0',
+        subData: [
+          {
+            id: 2,
+            title: '作品名称2',
+            number: 2500,
+            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+          },
+          {
+            id: 1,
+            title: '作品名称1',
+            number: 420,
+            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+          },
+          {
+            id: 3,
+            title: '作品名称3',
+            number: 100,
+            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+          }
+        ]
+      },
+      {
+        id: 9,
+        src: '/assets/imgs/designer-garden/shejishi.jpg',
+        name: '设计师9',
+        experience: '3年',
+        style: '传统中式',
+        place: '民用家具',
+        score: '9.0',
+        subData: [
+          {
+            id: 1,
+            title: '作品名称1',
+            number: 420,
+            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+          },
+          {
+            id: 3,
+            title: '作品名称3',
+            number: 100,
+            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+          },
+          {
+            id: 2,
+            title: '作品名称2',
+            number: 2500,
+            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+          }
+        ]
+      }
+    ]
+  },
+
+  // 家具论坛
+  FF: {
+    title: '家具论坛',
+    enTitle: 'FURNITURE FORUM',
+    data: [
+      {
+        id: 1,
+        title: '家具论坛标题1',
+        date: '2019-05-06',
+        src: '/assets/imgs/designer-garden/luntan.jpg'
+      },
+      {
+        id: 2,
+        title: '家具论坛标题2',
+        date: '2019-05-06',
+        src: '/assets/imgs/designer-garden/luntan.jpg'
+      },
+      {
+        id: 3,
+        title: '家具论坛标题3',
+        date: '2019-05-06',
+        src: '/assets/imgs/designer-garden/luntan.jpg'
+      },
+      {
+        id: 4,
+        title: '家具论坛标题4',
+        date: '2019-05-06',
+        src: '/assets/imgs/designer-garden/luntan.jpg'
+      }
+    ]
+  }
 
 };
