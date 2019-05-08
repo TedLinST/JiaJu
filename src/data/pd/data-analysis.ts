8f24060a8a90f5d2e950dd1c915f258636c6266f
HPDaniel <1028854519@qq.com>
2019-05-08 17:55:58 +0800

    大数据

--- src/data/pd/data-analysis.ts
+++ src/data/pd/data-analysis.ts
@@ -1,6 +1,7 @@
 // 大数据分析数据助手
 export const DataDataAnalysisHandler = {
 
+  
   // 企业内部数据分析案例
   IDAC: {
     title: '企业内部数据分析案例',
@@ -35,32 +36,32 @@ export const DataDataAnalysisHandler = {
       {
         id: 1,
         src: '/assets/imgs/data-analysis/fenxi-4.jpg',
-        title: '',
-        text: '从国内到海外，社交电商下一张该怎么走从国内到海外， 社交电商下一张该怎么走？',
+        title: '定制家具增长乏力，集成整装会成为新热点？',
+        text: '3月春暖花开，家具行业迎来3•15、展会、营销峰会、新品发布会等促销节点。 对于定制家具业来说，唯有忙碌的脚步方能应对2019的不确定性，还要适应全屋定制业务增速大幅下降的新常态，以及以多种形式突围企业经营成长的困境。',
         author: 'admin',
         time: '1小时前',
       },
       {
         id: 2,
-        src: '/assets/imgs/data-analysis/fenxi-4.jpg',
-        title: '',
-        text: '从国内到海外，社交电商下一张该怎么走从国内到海外， 社交电商下一张该怎么走？',
+        src: '/assets/imgs/data-analysis/fenxi-5.jpg',
+        title: '解构广东家具展，行业回归理性曲线',
+        text: '家具展上一个值得注意的现象是，几乎所有的家具人都在回归理性，脱衣服的少了，搞噱头的没了，喊口号的弱了。行业中少了自欺欺人，我们见到的更多的是对产品设计的认真，对经销扶持的认真，对政策落地的认真，对希望把握未来的认真。',
         author: 'admin',
         time: '1小时前',
       },
       {
         id: 3,
-        src: '/assets/imgs/data-analysis/fenxi-4.jpg',
-        title: '',
-        text: '从国内到海外，社交电商下一张该怎么走从国内到海外， 社交电商下一张该怎么走？',
+        src: '/assets/imgs/data-analysis/fenxi-6.jpg',
+        title: '定制家具正陷入与传统家具一样的低利润陷阱？',
+        text: '短短十几年间，“定制家具”成为新一代消费者时髦生活的标配，但当下的定制家具市场已不再是之前那片蓝海。 随着资本进入大型定制家具企业，定制家具智能制造的门槛越来越高，行业发展呈现两极分化，强者更强。从国内到海外，社交电商下一张该怎么走从国内到海外， 社交电商下一张该怎么走？',
         author: 'admin',
         time: '1小时前',
       },
       {
         id: 4,
-        src: '/assets/imgs/data-analysis/fenxi-4.jpg',
-        title: '',
-        text: '从国内到海外，社交电商下一张该怎么走从国内到海外， 社交电商下一张该怎么走？',
+        src: '/assets/imgs/data-analysis/fenxi-7.jpg',
+        title: '不同规模的家具企业如何进行转型升级？',
+        text: '在我国家具业现代化的转型中，中、小型企业可以产业集聚的方式，实现“专业化分工，社会化协作”来建立现代产业体系。从国内到海外，社交电商下一张该怎么走从国内到海外， 社交电商下一张该怎么走？',
         author: 'admin',
         time: '1小时前',
       }
@@ -76,19 +77,19 @@ export const DataDataAnalysisHandler = {
         id: 1,
         title: '阿里指数',
         date: '2019-04-24',
-        src: '/assets/imgs/data-analysis/fenxi-5.jpg'
+        src: '/assets/imgs/data-analysis/fenxi-14.jpg'
       },
       {
         id: 2,
         title: '百度指数',
         date: '2019-04-25',
-        src: '/assets/imgs/data-analysis/fenxi-6.jpg'
+        src: '/assets/imgs/data-analysis/fenxi-15.jpg'
       },
       {
         id: 3,
-        title: '腾讯指数',
+        title: '搜狗指数',
         date: '2019-04-25',
-        src: '/assets/imgs/data-analysis/fenxi-7.jpg'
+        src: '/assets/imgs/data-analysis/fenxi-16.jpg'
       }
     ]
   }
