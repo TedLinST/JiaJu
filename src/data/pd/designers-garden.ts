5de4e65abd5aa4c617041aed4f227455c843e399
qsqyyyf <931892083@qq.com>
2019-05-09 11:37:20 +0800

    设计师园地页面

--- src/data/pd/designers-garden.ts
+++ src/data/pd/designers-garden.ts
@@ -10,45 +10,45 @@ export const DataDesignersGardenHandler = {
         data: [
           {
             id: 1,
-            title: '设计师名称',
+            title: 'Dieter Rams',
             number: 1,
             rank: 1,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-1.jpg'
           },
           {
             id: 2,
-            title: '设计师名称',
+            title: 'YRJO KUKKAPURO',
             number: 2,
             rank: 1,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-2.jpg'
           },
           {
             id: 3,
-            title: '设计师名称',
+            title: 'はら けんや（原研哉）',
             number: 3,
             rank: 1,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-3.jpg'
           },
           {
             id: 4,
-            title: '设计师名称',
+            title: '朱小杰',
             number: 4,
             rank: 1,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-4.jpg'
           },
           {
             id: 5,
-            title: '设计师名称',
+            title: '侯正光',
             number: 5,
             rank: 1,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-5.jpg'
           },
           {
             id: 6,
-            title: '设计师名称',
+            title: '赖亚楠',
             number: 6,
             rank: 1,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-6.jpg'
           }
         ]
       },
@@ -57,45 +57,45 @@ export const DataDesignersGardenHandler = {
         data: [
           {
             id: 1,
-            title: '设计师名称',
+            title: '温浩',
             number: 1,
             rank: 2,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-7.jpg'
           },
           {
             id: 2,
-            title: '设计师名称',
+            title: '石大宇',
             number: 2,
             rank: 2,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-8.jpg'
           },
           {
             id: 3,
-            title: '设计师名称',
+            title: '张雷',
             number: 3,
             rank: 2,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-9.jpg'
           },
           {
             id: 4,
-            title: '设计师名称',
+            title: '于历战',
             number: 4,
             rank: 2,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-10.jpg'
           },
           {
             id: 5,
-            title: '设计师名称',
+            title: '吕永忠',
             number: 5,
             rank: 2,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-11.jpg'
           },
           {
             id: 6,
-            title: '设计师名称',
+            title: '周宸宸',
             number: 6,
             rank: 2,
-            src: '/assets/imgs/designer-garden/shejishi.jpg'
+            src: '/assets/imgs/designer-garden/designer-12.jpg'
           }
         ]
       }
@@ -109,8 +109,8 @@ export const DataDesignersGardenHandler = {
     data: [
       {
         id: 1,
-        src: '/assets/imgs/designer-garden/shejishi.jpg',
-        name: '设计师1',
+        src: '/assets/imgs/designer-garden/designer-13.jpg',
+        name: 'YRJO KUKKAPURO',
         experience: '5年',
         style: '传统中式',
         place: '民用家具',
@@ -120,26 +120,26 @@ export const DataDesignersGardenHandler = {
             id: 1,
             title: '作品名称1',
             number: 420,
-            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+            src: '/assets/imgs/designer-garden/works-1.jpg'
           },
           {
             id: 2,
             title: '作品名称2',
             number: 2500,
-            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+            src: '/assets/imgs/designer-garden/works-2.jpg'
           },
           {
             id: 3,
             title: '作品名称3',
             number: 100,
-            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+            src: '/assets/imgs/designer-garden/works-3.jpg'
           }
         ]
       },
       {
         id: 2,
-        src: '/assets/imgs/designer-garden/shejishi.jpg',
-        name: '设计师2',
+        src: '/assets/imgs/designer-garden/designer-14.jpg',
+        name: 'はら けんや（原研哉）',
         experience: '2年',
         style: '传统中式',
         place: '民用家具',
@@ -149,26 +149,26 @@ export const DataDesignersGardenHandler = {
             id: 1,
             title: '作品名称1',
             number: 420,
-            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+            src: '/assets/imgs/designer-garden/works-4.jpg'
           },
           {
             id: 2,
             title: '作品名称2',
             number: 2500,
-            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+            src: '/assets/imgs/designer-garden/works-5.jpg'
           },
           {
             id: 3,
             title: '作品名称3',
             number: 100,
-            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+            src: '/assets/imgs/designer-garden/works-6.jpg'
           }
         ]
       },
       {
         id: 3,
-        src: '/assets/imgs/designer-garden/shejishi.jpg',
-        name: '设计师3',
+        src: '/assets/imgs/designer-garden/designer-15.jpg',
+        name: '朱小杰',
         experience: '4年',
         style: '传统中式',
         place: '民用家具',
@@ -178,26 +178,26 @@ export const DataDesignersGardenHandler = {
             id: 1,
             title: '作品名称1',
             number: 420,
-            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+            src: '/assets/imgs/designer-garden/works-7.jpg'
           },
           {
             id: 2,
             title: '作品名称2',
             number: 2500,
-            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+            src: '/assets/imgs/designer-garden/works-8.jpg'
           },
           {
             id: 3,
             title: '作品名称3',
             number: 100,
-            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+            src: '/assets/imgs/designer-garden/works-9.jpg'
           }
         ]
       },
       {
         id: 4,
-        src: '/assets/imgs/designer-garden/shejishi.jpg',
-        name: '设计师4',
+        src: '/assets/imgs/designer-garden/designer-16.jpg',
+        name: '侯正光',
         experience: '3年',
         style: '传统中式',
         place: '民用家具',
@@ -207,26 +207,26 @@ export const DataDesignersGardenHandler = {
             id: 1,
             title: '作品名称1',
             number: 420,
-            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+            src: '/assets/imgs/designer-garden/works-10.jpg'
           },
           {
             id: 2,
             title: '作品名称2',
             number: 2500,
-            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+            src: '/assets/imgs/designer-garden/works-11.jpg'
           },
           {
             id: 3,
             title: '作品名称3',
             number: 100,
-            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+            src: '/assets/imgs/designer-garden/works-12.jpg'
           }
         ]
       },
       {
         id: 5,
-        src: '/assets/imgs/designer-garden/shejishi.jpg',
-        name: '设计师5',
+        src: '/assets/imgs/designer-garden/designer-17.jpg',
+        name: '赖亚楠',
         experience: '3年',
         style: '传统中式',
         place: '民用家具',
@@ -236,26 +236,26 @@ export const DataDesignersGardenHandler = {
             id: 1,
             title: '作品名称1',
             number: 420,
-            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+            src: '/assets/imgs/designer-garden/works-13.jpg'
           },
           {
             id: 2,
             title: '作品名称2',
             number: 2500,
-            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+            src: '/assets/imgs/designer-garden/works-14.jpg'
           },
           {
             id: 3,
             title: '作品名称3',
             number: 100,
-            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+            src: '/assets/imgs/designer-garden/works-15.jpg'
           }
         ]
       },
       {
         id: 6,
-        src: '/assets/imgs/designer-garden/shejishi.jpg',
-        name: '设计师6',
+        src: '/assets/imgs/designer-garden/designer-18.jpg',
+        name: '齐浩',
         experience: '3年',
         style: '传统中式',
         place: '民用家具',
@@ -265,26 +265,26 @@ export const DataDesignersGardenHandler = {
             id: 1,
             title: '作品名称1',
             number: 420,
-            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+            src: '/assets/imgs/designer-garden/works-16.jpg'
           },
           {
             id: 3,
             title: '作品名称3',
             number: 100,
-            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+            src: '/assets/imgs/designer-garden/works-17.jpg'
           },
           {
             id: 2,
             title: '作品名称2',
             number: 2500,
-            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+            src: '/assets/imgs/designer-garden/works-18.jpg'
           }
         ]
       },
       {
         id: 7,
-        src: '/assets/imgs/designer-garden/shejishi.jpg',
-        name: '设计师7',
+        src: '/assets/imgs/designer-garden/designer-19.jpg',
+        name: '王玉波',
         experience: '3年',
         style: '传统中式',
         place: '民用家具',
@@ -294,26 +294,26 @@ export const DataDesignersGardenHandler = {
             id: 3,
             title: '作品名称3',
             number: 100,
-            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+            src: '/assets/imgs/designer-garden/works-19.jpg'
           },
           {
             id: 1,
             title: '作品名称1',
             number: 420,
-            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+            src: '/assets/imgs/designer-garden/works-20.jpg'
           },
           {
             id: 2,
             title: '作品名称2',
             number: 2500,
-            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+            src: '/assets/imgs/designer-garden/works-21.jpg'
           }
         ]
       },
       {
         id: 8,
-        src: '/assets/imgs/designer-garden/shejishi.jpg',
-        name: '设计师8',
+        src: '/assets/imgs/designer-garden/designer-20.jpg',
+        name: '刘兴宝',
         experience: '3年',
         style: '传统中式',
         place: '民用家具',
@@ -323,26 +323,26 @@ export const DataDesignersGardenHandler = {
             id: 2,
             title: '作品名称2',
             number: 2500,
-            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+            src: '/assets/imgs/designer-garden/works-22.jpg'
           },
           {
             id: 1,
             title: '作品名称1',
             number: 420,
-            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+            src: '/assets/imgs/designer-garden/works-23.jpg'
           },
           {
             id: 3,
             title: '作品名称3',
             number: 100,
-            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+            src: '/assets/imgs/designer-garden/works-24.jpg'
           }
         ]
       },
       {
         id: 9,
-        src: '/assets/imgs/designer-garden/shejishi.jpg',
-        name: '设计师9',
+        src: '/assets/imgs/designer-garden/designer-21.jpg',
+        name: '刘磊',
         experience: '3年',
         style: '传统中式',
         place: '民用家具',
@@ -352,19 +352,19 @@ export const DataDesignersGardenHandler = {
             id: 1,
             title: '作品名称1',
             number: 420,
-            src: '/assets/imgs/designer-garden/zuopin-1.jpg'
+            src: '/assets/imgs/designer-garden/works-25.jpg'
           },
           {
             id: 3,
             title: '作品名称3',
             number: 100,
-            src: '/assets/imgs/designer-garden/zuopin-3.jpg'
+            src: '/assets/imgs/designer-garden/works-26.jpg'
           },
           {
             id: 2,
             title: '作品名称2',
             number: 2500,
-            src: '/assets/imgs/designer-garden/zuopin-2.jpg'
+            src: '/assets/imgs/designer-garden/works-27.jpg'
           }
         ]
       }
@@ -378,27 +378,27 @@ export const DataDesignersGardenHandler = {
     data: [
       {
         id: 1,
-        title: '家具论坛标题1',
+        title: '造型设计',
         date: '2019-05-06',
-        src: '/assets/imgs/designer-garden/luntan.jpg'
+        src: '/assets/imgs/designer-garden/forum-1.jpg'
       },
       {
         id: 2,
-        title: '家具论坛标题2',
+        title: '结构设计',
         date: '2019-05-06',
-        src: '/assets/imgs/designer-garden/luntan.jpg'
+        src: '/assets/imgs/designer-garden/forum-2.jpg'
       },
       {
         id: 3,
-        title: '家具论坛标题3',
+        title: '软装设计',
         date: '2019-05-06',
-        src: '/assets/imgs/designer-garden/luntan.jpg'
+        src: '/assets/imgs/designer-garden/forum-3.jpg'
       },
       {
         id: 4,
-        title: '家具论坛标题4',
+        title: '家具材料',
         date: '2019-05-06',
-        src: '/assets/imgs/designer-garden/luntan.jpg'
+        src: '/assets/imgs/designer-garden/forum-4.jpg'
       }
     ]
   }
