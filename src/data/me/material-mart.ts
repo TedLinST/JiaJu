c558f43862546bf4cbce32d28a2b4645b932b013
zkey <774508556@qq.com>
2019-05-15 14:57:46 +0800

    调整

--- src/data/me/material-mart.ts
+++ src/data/me/material-mart.ts
@@ -41,47 +41,7 @@ export const DataMaterialMartHandler = {
           label: '条件2.4'
         }
       ]
-    },
-    {
-      id: '3',
-      title: '条件三',
-      data: [
-        {
-          id: '1',
-          label: '条件3.1',
-          subData: [
-            {
-              id: '1',
-              label: '条件3.1.1'
-            },
-            {
-              id: '2',
-              label: '条件3.1.2'
-            },
-            {
-              id: '3',
-              label: '条件3.1.3'
-            },
-          ]
-        },
-        {
-          id: '2',
-          label: '条件3.2'
-        },
-        {
-          id: '3',
-          label: '条件3.3'
-        },
-        {
-          id: '4',
-          label: '条件3.4'
-        },
-        {
-          id: '5',
-          label: '条件3.5'
-        }
-      ]
-    },
+    }
   ],
 
   // 列表数据
@@ -92,10 +52,10 @@ export const DataMaterialMartHandler = {
       subTitle: '桐木板材',
       area: '山东菏泽',
       type: '板材',
-      specs: '1200*12mm',//1200*42*12mm
-      width: '42mm',
-      price: '￥2000/立方米',
-      qty: '40',//数量：50；报团数量：40。
+      specs: '1200*42*12mm',
+      price: '2000元/立方米',
+      qty: '40',
+      totalQty: '50',
       src: 'assets/imgs/material-mart/shangcheng-1.jpg'
     },
     {
@@ -104,10 +64,10 @@ export const DataMaterialMartHandler = {
       subTitle: '白蜡木方材',
       area: '山东滨州',
       type: '方材',
-      specs: '150*2000mm',//150*200*2000mm
-      width: '200mm',
-      price: '￥3000/立方米',
-      qty: '50',//数量：90；报团数量：50。
+      specs: '150*200*2000mm',
+      price: '3000元/立方米',
+      qty: '50',
+      totalQty: '90',
       src: 'assets/imgs/material-mart/shangcheng-2.jpg'
     },
     {
@@ -116,10 +76,10 @@ export const DataMaterialMartHandler = {
       subTitle: '铁杉方材',
       area: '美国',
       type: '方材',
-      specs: '100*2000mm',//100*350*2000mm
-      width: '350mm',
-      price: '￥1400/立方米',
-      qty: '20',//数量：40；报团数量：20。
+      specs: '100*350*2000mm',
+      price: '1400元/立方米',
+      qty: '20',
+      totalQty: '40',
       src: 'assets/imgs/material-mart/shangcheng-3.jpg'
     },
     {
@@ -128,10 +88,10 @@ export const DataMaterialMartHandler = {
       subTitle: '樟子松方材',
       area: '美国',
       type: '方材',
-      specs: '200*2000mm',//200*350*2000mm
-      width: '350mm',
-      price: '￥1350/立方米',
-      qty: '20',//数量：50；报团数量：20。
+      specs: '200*350*2000mm',
+      price: '1350元/立方米',
+      qty: '20',
+      totalQty: '50',
       src: 'assets/imgs/material-mart/shangcheng-4.jpg'
     },
     {
@@ -140,10 +100,10 @@ export const DataMaterialMartHandler = {
       subTitle: '白松方材',
       area: '美国',
       type: '方材',
-      specs: '200*2000mm',//200*350*2000mm
-      width: '350mm',
-      price: '￥1200/立方米',
-      qty: '50',//数量：80；报团数量：50。
+      specs: '200*350*2000mm',
+      price: '1200元/立方米',
+      qty: '50',
+      totalQty: '80',
       src: 'assets/imgs/material-mart/shangcheng-5.jpg'
     },
     {
@@ -152,10 +112,10 @@ export const DataMaterialMartHandler = {
       subTitle: '奥松板材',
       area: '美国',
       type: '方材',
-      specs: '2440*20mm',//1220*2440*20mm
-      width: '1220mm',
-      price: '￥130/张',
-      qty: '30',//数量：90；报团数量：30。
+      specs: '1220*2440*20mm',
+      price: '130元/张',
+      qty: '30',
+      totalQty: '90',
       src: 'assets/imgs/material-mart/shangcheng-6.jpg'
     },
     {
@@ -164,10 +124,10 @@ export const DataMaterialMartHandler = {
       subTitle: '黄松方材',
       area: '美国',
       type: '方材',
-      specs: '300*2000mm',//200*300*2000mm
-      width: '200mm',
-      price: '￥1420/立方米',
-      qty: '20',//数量：50；报团数量：20。
+      specs: '200*300*2000mm',
+      price: '1420元/立方米',
+      qty: '20',
+      totalQty: '50',
       src: 'assets/imgs/material-mart/shangcheng-7.jpg'
     },
     {
@@ -176,10 +136,10 @@ export const DataMaterialMartHandler = {
       subTitle: '相思木板材',
       area: '海南海口',
       type: '板材',
-      specs: '20*1000mm',//20*300*1000mm
-      width: '300mm',
-      price: '￥2200/立方米',
-      qty: '10',//数量：40；报团数量：10。
+      specs: '20*300*1000mm',
+      price: '2200元/立方米',
+      qty: '10',
+      totalQty: '40',
       src: 'assets/imgs/material-mart/shangcheng-8.jpg'
     },
     {
@@ -188,10 +148,10 @@ export const DataMaterialMartHandler = {
       subTitle: '缅甸瓦城柚木',
       area: '缅甸瓦城',
       type: '板材',
-      specs: '20*1000mm',//20*300*1000mm
-      width: '300mm',
-      price: '￥250000/立方米',
-      qty: '20',//数量：55；报团数量：20。
+      specs: '20*300*1000mm',
+      price: '250000元/立方米',
+      qty: '20',
+      totalQty: '55',
       src: 'assets/imgs/material-mart/shangcheng-9.jpg'
     },
     {
@@ -200,10 +160,10 @@ export const DataMaterialMartHandler = {
       subTitle: '杨木方材',
       area: '湖南长沙',
       type: '方材',
-      specs: '20*1000mm',//20*300*1000mm
-      width: '300mm',
-      price: '￥800/立方米',
-      qty: '30',//数量：95；报团数量：30。
+      specs: '20*300*1000mm',
+      price: '800元/立方米',
+      qty: '30',
+      totalQty: '95',
       src: 'assets/imgs/material-mart/shangcheng-10.jpg'
     },
     {
@@ -212,10 +172,10 @@ export const DataMaterialMartHandler = {
       subTitle: '榉木板材',
       area: '俄罗斯',
       type: '板材',
-      specs: '40*2000mm',//40*350*2000mm
-      width: '350mm',
-      price: '￥2500/立方米',
-      qty: '20',//数量：40；报团数量：20。
+      specs: '40*350*2000mm',
+      price: '2500元/立方米',
+      qty: '20',
+      totalQty: '40',
       src: 'assets/imgs/material-mart/shangcheng-11.jpg'
     },
     {
@@ -224,10 +184,10 @@ export const DataMaterialMartHandler = {
       subTitle: '桤木板材',
       area: '四川成都',
       type: '板材',
-      specs: '30*1000mm',//30*150*1000mm
-      width: '150mm',
-      price: '￥700/立方米',
-      qty: '55',//数量：80；报团数量：55。
+      specs: '30*150*1000mm',
+      price: '700元/立方米',
+      qty: '55',
+      totalQty: '80',
       src: 'assets/imgs/material-mart/shangcheng-12.jpg'
     },
     {
@@ -236,10 +196,10 @@ export const DataMaterialMartHandler = {
       subTitle: '柏木板材',
       area: '四川成都',
       type: '板材',
-      specs: '30*1000mm',//30*150*1000mm
-      width: '150mm',
-      price: '￥700/立方米',
-      qty: '20',//数量：70；报团数量：20。
+      specs: '30*150*1000mm',
+      price: '700元/立方米',
+      qty: '20',
+      totalQty: '70',
       src: 'assets/imgs/material-mart/shangcheng-13.jpg'
     },
     {
@@ -248,10 +208,10 @@ export const DataMaterialMartHandler = {
       subTitle: '白蜡板材',
       area: '美国',
       type: '板材',
-      specs: '40*2000mm',//40*350*2000mm
-      width: '350mm',
-      price: '￥3500/立方米',
-      qty: '20',//数量：40；报团数量：20。
+      specs: '40*350*2000mm',
+      price: '3500元/立方米',
+      qty: '20',
+      totalQty: '40',
       src: 'assets/imgs/material-mart/shangcheng-14.jpg'
     },
     {
@@ -260,10 +220,10 @@ export const DataMaterialMartHandler = {
       subTitle: '白松方材',
       area: '美国',
       type: '方材',
-      specs: '100*3800mm',//100*100*3800mm
-      width: '100mm',
-      price: '￥1300/立方米',
-      qty: '20',//数量：80；报团数量：20。
+      specs: '100*100*3800mm',
+      price: '1300元/立方米',
+      qty: '20',
+      totalQty: '80',
       src: 'assets/imgs/material-mart/shangcheng-15.jpg'
     },
     {
@@ -272,10 +232,10 @@ export const DataMaterialMartHandler = {
       subTitle: '黄杨木板材',
       area: '美国',
       type: '板材',
-      specs: '30*2500mm',//30*200*2500mm
-      width: '200mm',
-      price: '￥2200/立方米',
-      qty: '20',//数量：40；报团数量：20。
+      specs: '30*200*2500mm',
+      price: '2200元/立方米',
+      qty: '20',
+      totalQty: '40',
       src: 'assets/imgs/material-mart/shangcheng-16.jpg'
     },
     {
@@ -284,10 +244,10 @@ export const DataMaterialMartHandler = {
       subTitle: '欧罗斯松木板材',
       area: '俄罗斯',
       type: '板材',
-      specs: '20*2000mm',//20*300*2000mm
-      width: '300mm',
-      price: '￥1000/立方米',
-      qty: '10',//数量：20；报团数量：10。
+      specs: '20*300*2000mm',
+      price: '1000元/立方米',
+      qty: '10',
+      totalQty: '20',
       src: 'assets/imgs/material-mart/shangcheng-17.jpg'
     },
     {
@@ -296,10 +256,10 @@ export const DataMaterialMartHandler = {
       subTitle: '樟子松板材',
       area: '俄罗斯',
       type: '板材',
-      specs: '20*2000mm',//20*300*2000mm
-      width: '300mm',
-      price: '￥2000/立方米',
-      qty: '55',//数量：75；报团数量：55。
+      specs: '20*300*2000mm',
+      price: '2000元/立方米',
+      qty: '55',
+      totalQty: '75',
       src: 'assets/imgs/material-mart/shangcheng-18.jpg'
     },
     {
@@ -308,10 +268,10 @@ export const DataMaterialMartHandler = {
       subTitle: '欧洲云杉原木',
       area: '捷克',
       type: '原木',
-      specs: '300*3000mm',//300*300*3000mm
-      width: '300mm',
-      price: '￥1300/立方米',
-      qty: '25',//数量：50；报团数量：25。
+      specs: '300*300*3000mm',
+      price: '1300元/立方米',
+      qty: '25',
+      totalQty: '50',
       src: 'assets/imgs/material-mart/shangcheng-19.jpg'
     },
     {
@@ -320,12 +280,12 @@ export const DataMaterialMartHandler = {
       subTitle: '橡木拼板板材',
       area: '美国',
       type: '板材',
-      specs: '2440*18mm',//1220*2440*18mm
-      width: '1220mm',
-      price: '￥3200/立方米',
-      qty: '10',//数量：50；报团数量：10。
+      specs: '1220*2440*18mm',
+      price: '3200元/立方米',
+      qty: '10',
+      totalQty: '50',
       src: 'assets/imgs/material-mart/shangcheng-20.jpg'
     },
-  
+
   ]
 };
