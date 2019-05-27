2551cf0f6108df2c024a76d4be1bf5a46fb1572c
qsqyyyf <931892083@qq.com>
2019-05-27 10:04:17 +0800

    互动讲堂

--- src/data/sec/interaction-forum.ts
+++ src/data/sec/interaction-forum.ts
@@ -27,24 +27,24 @@ export const DataInteractionForumHandler = {
     data1: [
       {
         id: '1',
-        BiaoTi: '互动讲堂预告',
-        ZhaiYao: '摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容',
-        KaiShiShiJian: '2019-05-14',
-        src: 'assets/imgs/interaction-forum/yugao.jpg'
+        BiaoTi: 'C-1 Chemistry of biomass energy conversion and chemical synthesis',
+        ZhaiYao: '应材料科学与工程学院、国家林业局生物乙醇研究中心、湖南省木本生物质转化工程技术研究中心的邀请，日本富山大学椿范立教授于2019年5月6日访问我院，并为我院生物质能源与材料、林化、化工相关学科的师生做题为“C-1 Chemistry of biomass energy conversion and chemical synthesis”的学术报告。',
+        KaiShiShiJian: '2019年5月7日',
+        src: 'assets/imgs/interaction-forum/notice-1.jpg'
       },
       {
         id: '2',
-        BiaoTi: '互动讲堂预告',
-        ZhaiYao: '摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容',
-        KaiShiShiJian: '2019-05-14',
-        src: 'assets/imgs/interaction-forum/yugao.jpg'
+        BiaoTi: 'Creating super hydrophobic surface using natural cellulose nanomaterials',
+        ZhaiYao: '中南林业科技大学材料科学与工程学院于逸夫楼546举办美国田纳西大学王思群教授学术报告会，材料学院院长李贤军教授，副院长袁光明教授，相关学科教研室老师，全体在校研究生以及部分本科生参加本次报告会。',
+        KaiShiShiJian: '2018年7月3日',
+        src: 'assets/imgs/interaction-forum/notice-2.jpg'
       },
       {
         id: '3',
-        BiaoTi: '互动讲堂预告',
-        ZhaiYao: '摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容摘要内容',
-        KaiShiShiJian: '2019-05-14',
-        src: 'assets/imgs/interaction-forum/yugao.jpg'
+        BiaoTi: '定制家居智能制造',
+        ZhaiYao: '索菲亚家居副总裁张挺先生为家居学院师生做了“定制家居智能制造”的专题讲座。围绕“定制家居行业现状、定制家居智能制造内涵、索菲亚家居智能制造之路以及索菲亚智能制造示范线-4.0工厂”等方面，张总以自己在家具智能制造领域的亲身经历与实践，为大家奉献了一场精彩纷呈的家具智能制造前沿技术分享。',
+        KaiShiShiJian: '2019年4月28日',
+        src: 'assets/imgs/interaction-forum/notice-3.jpg'
       }
     ]
   },
@@ -101,43 +101,43 @@ export const DataInteractionForumHandler = {
     LIST_DATA: [
       {
         id: '1',
-        BiaoTi: '标题',
-        src: 'assets/imgs/interaction-forum/huigu-1.jpg'
+        BiaoTi: '红木家具材料，福建农林大学，林金国教授',
+        src: 'assets/imgs/interaction-forum/review-1.jpg'
       },
       {
         id: '2',
-        BiaoTi: '标题',
-        src: 'assets/imgs/interaction-forum/huigu-2.jpg'
+        BiaoTi: '明式家具，山东农业大学，董晓英',
+        src: 'assets/imgs/interaction-forum/review-2.jpg'
       },
       {
         id: '3',
-        BiaoTi: '标题',
-        src: 'assets/imgs/interaction-forum/huigu-3.jpg'
+        BiaoTi: '明清家具的古今加工方式，南京林业大学，赵中元',
+        src: 'assets/imgs/interaction-forum/review-3.jpg'
       },
       {
         id: '4',
-        BiaoTi: '标题',
-        src: 'assets/imgs/interaction-forum/huigu-1.jpg'
+        BiaoTi: '大数据科学与应用，清华大学，余凯',
+        src: 'assets/imgs/interaction-forum/review-4.jpg'
       },
       {
         id: '5',
-        BiaoTi: '标题',
-        src: 'assets/imgs/interaction-forum/huigu-2.jpg'
+        BiaoTi: '中国古典家具发展进程',
+        src: 'assets/imgs/interaction-forum/review-5.jpg'
       },
       {
         id: '6',
-        BiaoTi: '标题',
-        src: 'assets/imgs/interaction-forum/huigu-3.jpg'
+        BiaoTi: '小家具大境界，观复博物馆馆长，马未都',
+        src: 'assets/imgs/interaction-forum/review-6.jpg'
       },
       {
         id: '7',
-        BiaoTi: '标题',
-        src: 'assets/imgs/interaction-forum/huigu-1.jpg'
+        BiaoTi: '鼎盛时期的古典家具：明清家具的榫卯结构',
+        src: 'assets/imgs/interaction-forum/review-7.jpg'
       },
       {
         id: '8',
-        BiaoTi: '标题8',
-        src: 'assets/imgs/interaction-forum/huigu-2.jpg'
+        BiaoTi: '明清家具的选材用料',
+        src: 'assets/imgs/interaction-forum/review-8.jpg'
       }
     ]
   }
