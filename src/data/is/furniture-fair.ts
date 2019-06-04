3c1615921d46326021a207494e41fd648b577431
HPDaniel <1028854519@qq.com>
2019-06-04 15:08:26 +0800

    家具展会详情页面内容1

--- src/data/is/furniture-fair.ts
+++ src/data/is/furniture-fair.ts
@@ -8,49 +8,49 @@ export const DataFurnitureFairHandler = {
     // 展会详细介绍 
     data0: {
       title: '标题',
-      content: '内容内容内容内容内容内容内容内容内容',
-      src: 'assets/imgs/furniture-fair/zhanhui-1.jpg'
+      content: '深圳时尚家居设计周暨深圳国际家具展览会，是中国家具行业最成功的商业展，坚持“设计导向、潮流引领、持续创新”，以设计为纽带，与城市文化共融的深圳国际家具展，日益成为全球家具和设计界认识深圳的窗口，也成为“国际设计资源与中国制造连接”及“中国制造寻找国际、国内市场”的战略平台。',
+      src: 'assets/imgs/furniture-fair/ZHjieshao-1.jpg'
     },
     // 展会资讯
     data1: [
       {
         id: '1',
-        title: '展会资讯标题名称',
+        title: '青年设计师创想汇：给你一场超级的思想脑爆',
         time: '2019-05-22'
       },
       {
         id: '2',
-        title: '展会资讯标题名称',
+        title: '给设计以更靓丽的色彩——金堂奖全国女设计师联盟来了！',
         time: '2019-05-22'
       },
       {
         id: '3',
-        title: '展会资讯标题名称',
+        title: '中国青年设计师创想峰会拉开帷幕，系列活动引爆设计新势力',
         time: '2019-05-22'
       },
       {
         id: '4',
-        title: '展会资讯标题名称',
+        title: '当广州遇上深圳，设计界的华山论道就在中国青年设计师创想峰会',
         time: '2019-05-22'
       },
       {
         id: '5',
-        title: '展会资讯标题名称',
+        title: '荣耀降临：是谁摘得了第五届GOtrend金汐奖的桂冠 ？',
         time: '2019-05-22'
       },
       {
         id: '6',
-        title: '展会资讯标题名称',
+        title: '第一届 “中法设计之约”将于3月19-23日在深圳举办',
         time: '2019-05-22'
       },
       {
         id: '7',
-        title: '展会资讯标题名称',
+        title: '深圳时尚家居设计周，中国与世界预见未来的世纪之约',
         time: '2019-05-22'
       },
       {
         id: '8',
-        title: '展会资讯标题名称',
+        title: '彰显囯萃的国际表达 | 3月深圳展，见证封面"首映礼"',
         time: '2019-05-22'
       }
     ],
@@ -95,39 +95,39 @@ export const DataFurnitureFairHandler = {
     // 本届展会
     data0: [
       {
-        title: '123',
+        title: '中国青年设计师创想峰会拉开帷幕，系列活动引爆设计新势力',
         src: 'assets/imgs/furniture-fair/huigu-1.jpg'
       },
       {
-        title: '234',
+        title: '荣耀降临：是谁摘得了第五届GOtrend金汐奖的桂冠 ？',
         src: 'assets/imgs/furniture-fair/huigu-2.jpg'
       },
       {
-        title: '345',
-        src: 'assets/imgs/furniture-fair/huigu-1.jpg'
+        title: '第一届 “中法设计之约”将于3月19-23日在深圳举办',
+        src: 'assets/imgs/furniture-fair/huigu-3.jpg'
       },
       {
-        title: '456',
-        src: 'assets/imgs/furniture-fair/huigu-2.jpg'
+        title: '深圳时尚家居设计周，中国与世界预见未来的世纪之约',
+        src: 'assets/imgs/furniture-fair/huigu-4.jpg'
       }
     ],
     // 往届展会
     data1: [
       {
-        title: '123',
-        src: 'assets/imgs/furniture-fair/huigu-3.jpg'
+        title: '2018年',
+        src: 'assets/imgs/furniture-fair/huigu-5.jpg'
       },
       {
-        title: '234',
-        src: 'assets/imgs/furniture-fair/huigu-4.jpg'
+        title: '2017年',
+        src: 'assets/imgs/furniture-fair/huigu-6.jpg'
       },
       {
-        title: '345',
-        src: 'assets/imgs/furniture-fair/huigu-3.jpg'
+        title: '2016年',
+        src: 'assets/imgs/furniture-fair/huigu-7.jpg'
       },
       {
-        title: '456',
-        src: 'assets/imgs/furniture-fair/huigu-4.jpg'
+        title: '2015年',
+        src: 'assets/imgs/furniture-fair/huigu-8.jpg'
       }
     ]
   }
