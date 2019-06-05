8166781270efc24098cb3d5461477b1033cfb47b
HPDaniel <1028854519@qq.com>
2019-06-05 10:45:00 +0800

    工商服务详情页内容

--- src/data/is/patent-service.ts
+++ src/data/is/patent-service.ts
@@ -3,67 +3,79 @@ export const DataPatentServiceHandler = {
 
   // 子横幅
   SB: {
-    title: '国家知识产权局官方网站',
-    link: 'http://www.baidu.com',
-    src: 'assets/imgs/patent-service/zhuanli.jpg'
+    title: '国家市场监督管理总局网站',
+    link: 'http://www.samr.gov.cn/',
+    src: 'assets/imgs/patent-service/gongshang.jpg'
   },
 
-  // 专利政策与解读
+  // 专利政策与解读  ->  工商政策与解读
   PPI: {
-    title: '专利政策与解读',
+    title: '工商政策与解读',
     enTitle: 'PATENT POLICY AND INTERPRETATION',
     // 相关政策
     data0: [
       {
-        title: '我是一个XXXXXX标题'
+        title: '加强专利代理管理 促进规范行业健康发展'
       },
       {
-        title: '我是一个标题XXXXXXXXXX'
+        title: '全国人民代表大会常务委员会做出修改《中华人民共和国反不正当竞争法》的决定'
       },
       {
-        title: '我是一个X标题'
+        title: '“铁拳”出击 严厉打击侵犯知识产权违法行为'
       },
       {
-        title: '我是一个XXX标题'
+        title: '国家市场监督管理总局规章制定程序规定'
       },
       {
-        title: '我X是一个标题'
+        title: '市场监管总局等六部门印发《加强网购和进出口领域知识产权执法实施办法》'
       },
       {
-        title: 'XXXXX我是一个标题'
+        title: '专利代理管理办法'
       },
       {
-        title: '我是一个XXXXXXX标题'
+        title: '中华人民共和国价格法'
       },
       {
-        title: '我是一个标题'
+        title: '中华人民共和国反不正当竞争法'
+      },
+      {
+        title:'直销管理条例'
+      },
+      {
+        title:'价格违法行为行政处罚规定'
       }
     ],
     // 政策解读
     data1: [
       {
-        title: '我是一个XXXXXX标题'
+        title: '2018年前三季度市场环境形势分析报告'
       },
       {
-        title: '我是一个标题XXXXXXXXXX'
+        title: '市场监管总局关于明确免予办理强制性产品认证工作要求的通知'
       },
       {
-        title: '我是一个X标题'
+        title: '市场监管总局关于加强认证监管工作的通知'
       },
       {
-        title: '我是一个XXX标题'
+        title: '市场监管总局关于《市场监督管理投诉与举报处理办法（征求意见稿）》公开征求意见的公告'
       },
       {
-        title: '我X是一个标题'
+        title: '市场监管总局关于发布《绿色产品标识使用管理办法》的公告'
       },
       {
-        title: 'XXXXX我是一个标题'
+        title: '十三个新职业发布 人工智能工程技术人员、无人机驾驶员等榜上有名'
       },
       {
-        title: '我是一个XXXXXXX标题'
+        title: '市场监管总局：去年虚假违法广告案罚没金额超7亿元'
       },
       {
-        title: '我是一个标题'
+        title: '按摩器具产品抽查 超三成不合格存漏电火灾风险'
+      },
+      {
+        title: '2018年全国市场主体发展基本情况'
+      },
+      {
+        title: '全国12358价格监管平台2018年11月分析报告'
       }
     ]
   },
@@ -74,84 +86,84 @@ export const DataPatentServiceHandler = {
     enTitle: 'HANDLING GUIDELINE',
     data: [
       {
-        title: '公司名称',
+        title: '开办家具厂所需的手续',
         src: 'assets/imgs/patent-service/zhinan-1.jpg'
       },
       {
-        title: '公司名称',
+        title: '如何办理营业执照',
         src: 'assets/imgs/patent-service/zhinan-2.jpg'
       },
       {
-        title: '公司名称',
+        title: '企业公章的制作与管理',
         src: 'assets/imgs/patent-service/zhinan-1.jpg'
       },
       {
-        title: '公司名称',
+        title: '企业银行账户开户',
         src: 'assets/imgs/patent-service/zhinan-2.jpg'
       },
       {
-        title: '公司名称',
+        title: '国税地税的办理',
         src: 'assets/imgs/patent-service/zhinan-1.jpg'
       },
       {
-        title: '公司名称',
+        title: '企业社保登记',
         src: 'assets/imgs/patent-service/zhinan-2.jpg'
       },
       {
-        title: '公司名称',
+        title: '工商年检办理',
         src: 'assets/imgs/patent-service/zhinan-1.jpg'
       },
       {
-        title: '公司名称',
+        title: '家具出口手续办理',
         src: 'assets/imgs/patent-service/zhinan-2.jpg'
       }
     ]
   },
 
-  // 专利代理机构
+  // 专利代理机构  -> 工商代理机构
   PA: {
-    title: '专利代理机构',
+    title: '工商代理机构',
     enTitle: 'PATENT AGENCY',
     data: [
       {
-        title: '机构名称',
-        src: 'assets/imgs/patent-service/jigou-1.jpg'
+        title: '人人帮',
+        src: 'assets/imgs/patent-service/DLjigou-1.jpg'
       },
       {
-        title: '机构名称',
-        src: 'assets/imgs/patent-service/jigou-2.jpg'
+        title: '好办易',
+        src: 'assets/imgs/patent-service/DLjigou-2.jpg'
       },
       {
-        title: '机构名称',
-        src: 'assets/imgs/patent-service/jigou-3.jpg'
+        title: '韧启',
+        src: 'assets/imgs/patent-service/DLjigou-3.jpg'
       },
       {
-        title: '机构名称',
-        src: 'assets/imgs/patent-service/jigou-4.jpg'
+        title: '讯法网',
+        src: 'assets/imgs/patent-service/DLjigou-4.jpg'
       },
       {
-        title: '机构名称',
-        src: 'assets/imgs/patent-service/jigou-5.jpg'
+        title: '就爱创业',
+        src: 'assets/imgs/patent-service/DLjigou-5.jpg'
       },
       {
-        title: '机构名称',
-        src: 'assets/imgs/patent-service/jigou-1.jpg'
+        title: '顶呱呱',
+        src: 'assets/imgs/patent-service/DLjigou-6.jpg'
       },
       {
-        title: '机构名称',
-        src: 'assets/imgs/patent-service/jigou-2.jpg'
+        title: '慧算账',
+        src: 'assets/imgs/patent-service/DLjigou-7.jpg'
       },
       {
-        title: '机构名称',
-        src: 'assets/imgs/patent-service/jigou-3.jpg'
+        title: '京师企服',
+        src: 'assets/imgs/patent-service/DLjigou-8.jpg'
       },
       {
-        title: '机构名称',
-        src: 'assets/imgs/patent-service/jigou-4.jpg'
+        title: '和创财税',
+        src: 'assets/imgs/patent-service/DLjigou-9.jpg'
       },
       {
-        title: '机构名称',
-        src: 'assets/imgs/patent-service/jigou-5.jpg'
+        title: '誉胜',
+        src: 'assets/imgs/patent-service/DLjigou-10.jpg'
       }
     ]
   }
