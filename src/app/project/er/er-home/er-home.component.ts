a3203beb669c6b9e8208798f4456b555fa9a74fd
zkey <774508556@qq.com>
2019-06-06 18:04:30 +0800

    企业资源管理系统

--- src/app/project/er/er-home/er-home.component.ts
+++ src/app/project/er/er-home/er-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-er-home',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./er-home.component.scss']
 })
 export class ErHomeComponent implements OnInit {
+
+  @HostBinding("class.key-flex-component-v") flex: boolean = true;
 
   constructor() { }
 
