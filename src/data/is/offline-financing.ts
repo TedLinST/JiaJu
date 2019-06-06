0cd7ea734ff844104e2e58a83637b9154816a149
HPDaniel <1028854519@qq.com>
2019-06-06 10:23:33 +0800

    线下融资活动

--- src/data/is/offline-financing.ts
+++ src/data/is/offline-financing.ts
@@ -6,28 +6,28 @@ export const DataOfflineFinancingHandler = {
     enTitle: 'FINANCING INFORMATION',
     data: [
       {
-        BiaoTi: '融资资讯标题XXX',
-        NeiRong: '我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容',
+        BiaoTi: '家具企业投资越南，机遇与挑战并存的荆棘之路',
+        NeiRong: '家具企业投资越南成为热潮，机遇与挑战并存的荆棘之路：1)木材供应不足以满足家具制造业的需要；2)劳动力素质较低；3)法律体系不完善，外商面临风险；中美贸易战为跨境家具生产商带来新的不确定因素。',
         BiaoQian: '家具',
         FenLei: '家具周刊',
         FaBuShiJian: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-1.jpg'
+        src: 'assets/imgs/offline-financing/RZzixun-1.jpg'
       },
       {
-        BiaoTi: 'XXXX融资资讯标题',
-        NeiRong: '我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容',
+        BiaoTi: '大消费投资赛道排行榜：家居家具位居第二位',
+        NeiRong: '在大消费领域共1846次投资事件中，这三大领域位居前三位：①餐饮连锁：重新定义生活方式网红餐饮价值诱人；②家居家具：以数字化升级为主线重构供应链挖掘增量空间；③便利店：技术驱动服务创新商业空间向场外延展。',
         BiaoQian: '家具',
         FenLei: '家具周刊',
         FaBuShiJian: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-1.jpg'
+        src: 'assets/imgs/offline-financing/RZzixun-2.jpg'
       },
       {
-        BiaoTi: 'XXXXXX融资资讯标题XXXXXX',
-        NeiRong: '我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容',
+        BiaoTi: '共享家具品牌“抖抖家居”获300万元天使轮融资',
+        NeiRong: '亿欧7月17日首发消息，共享家具品牌“抖抖家居”获得300万元天使轮融资，投资方为美国纳斯达克上市公司“航美传媒”联合创始人。据悉，本轮融资将用于抖抖家居的平台建设、家居设计以及团队扩充等方面。',
         BiaoQian: '家具',
         FenLei: '家具周刊',
         FaBuShiJian: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-1.jpg'
+        src: 'assets/imgs/offline-financing/RZzixun-3.jpg'
       }
     ]
   },
@@ -45,22 +45,22 @@ export const DataOfflineFinancingHandler = {
     data0: [
       {
         FenLei: '融资微路演',
-        BiaoTi: '标题标题标题标题',
+        BiaoTi: '量身打造线下微路演，直面投资大咖，获取创业真经',
         src: 'assets/imgs/offline-financing/rongzi-2.jpg'
       },
       {
         FenLei: '融资沙龙',
-        BiaoTi: '标题标题标题标题',
+        BiaoTi: '线下24年对接经验，直通资本，助力融资，成就创业梦想',
         src: 'assets/imgs/offline-financing/rongzi-5.jpg'
       },
       {
         FenLei: '资本相亲汇',
-        BiaoTi: '标题标题标题标题',
+        BiaoTi: '以“8MINS / 8 VS 8”为活动亮点，平等互动、专业切磋，让资创双方零距离对话',
         src: 'assets/imgs/offline-financing/rongzi-4.jpg'
       },
       {
         FenLei: '创刻SHOW',
-        BiaoTi: '标题标题标题标题',
+        BiaoTi: '创立900秒路演形式，帮助创业者直面优质资本机构，精准搓配',
         src: 'assets/imgs/offline-financing/rongzi-5.jpg'
       }
     ],
@@ -68,232 +68,160 @@ export const DataOfflineFinancingHandler = {
     // 融资微路演
     data1: [
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '携手机构投资人，为创业者“赋能”（投融界第201期融资微路演成功举办）',
+        DiDian: '北京',
+        RiQi: '2019-05-31',
+        src: 'assets/imgs/offline-financing/weiluyan-1.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
+        BiaoTi: '微路演第200期“战场”，创投双方你来我往（投融界融资微路演第200期成功举办）',
+        DiDian: '上海',
+        RiQi: '2019-05-24',
+        src: 'assets/imgs/offline-financing/weiluyan-2.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-8.jpg'
+        BiaoTi: '深圳再出发，微路演助创业（投融界融资微路演第199期成功举办）',
+        DiDian: '广东 - 深圳',
+        RiQi: '2019-05-17',
+        src: 'assets/imgs/offline-financing/weiluyan-3.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
+        BiaoTi: '闭门路演，创业者与投资人的碰撞（投融界融资微路演第198期成功举办）',
+        DiDian: '浙江 - 杭州',
         RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        src: 'assets/imgs/offline-financing/weiluyan-4.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
+        BiaoTi: '成长黄金期，寻找“伙伴”快速发展（投融界第197期融资微路演成功举办）',
+        DiDian: '浙江 - 杭州',
         RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
+        src: 'assets/imgs/offline-financing/weiluyan-5.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
+        BiaoTi: '时刻准备着，融资不等待！（投融界融资微路演第196期成功举办）',
+        DiDian: '浙江 - 杭州',
         RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-8.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-8.jpg'
+        src: 'assets/imgs/offline-financing/weiluyan-6.jpg'
       }
     ],
 
     // 融资沙龙
     data2: [
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-8.jpg'
+        BiaoTi: '资本“大咖”聚上海，考察优质项目（投融界第409期融资沙龙成功举办）',
+        DiDian: '上海',
+        RiQi: '2019-06-05',
+        src: 'assets/imgs/offline-financing/shalong-1.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
+        BiaoTi: '赤子之心，乘风破浪为融资而来（投融界第408期融资沙龙成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-05-30',
+        src: 'assets/imgs/offline-financing/shalong-2.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '与投资人自由交流，求取创业真经（投融界第407期融资沙龙成功举办）',
+        DiDian: '上海',
+        RiQi: '2019-05-23',
+        src: 'assets/imgs/offline-financing/shalong-3.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
+        BiaoTi: '延续520，资创“看对眼” （投融界第406期融资沙龙成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-05-21',
+        src: 'assets/imgs/offline-financing/shalong-4.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '越努力越优秀，青年创业者大显身手（投融界第405期融资沙龙成功举办）',
+        DiDian: '上海',
+        RiQi: '2019-05-16',
+        src: 'assets/imgs/offline-financing/shalong-5.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-9.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '三十余家投资机构，与创业精英共商合作 （投融界第404期融资沙龙成功举办',
+        DiDian: '上海',
+        RiQi: '2019-05-09',
+        src: 'assets/imgs/offline-financing/shalong-6.jpg'
       }
     ],
 
     // 资本相亲汇
     data3: [
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
+        BiaoTi: '双创活动周来临，资本相亲汇走起（投融界第23期资本相亲汇成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-06-04',
+        src: 'assets/imgs/offline-financing/xiangqinhui-1.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
+        BiaoTi: '零距离面谈投资人，为创业赋能（投融界第22期资本相亲汇成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-05-29',
+        src: 'assets/imgs/offline-financing/xiangqinhui-2.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '一对一洽谈，他们为融资而来！（投融界第21期资本相亲汇成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-05-22',
+        src: 'assets/imgs/offline-financing/xiangqinhui-3.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-8.jpg'
+        BiaoTi: '资创初见，他们是这样洽谈的 （投融界第20期资本相亲汇成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-05-15',
+        src: 'assets/imgs/offline-financing/xiangqinhui-4.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
+        BiaoTi: '创业宜早不宜迟，融资正当时（投融界第19期资本相亲汇成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-05-08',
+        src: 'assets/imgs/offline-financing/xiangqinhui-5.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '一对一洽谈，线下“相亲”投资人（投融界第18期资本相亲汇成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-04-29',
+        src: 'assets/imgs/offline-financing/xiangqinhui-6.jpg'
       }
     ],
 
     // 创刻SHOW
     data4: [
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-8.jpg'
+        BiaoTi: '科技自立，AI专场路演气氛火热（投融界第47期创刻SHOW成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-05-22',
+        src: 'assets/imgs/offline-financing/chuangke-1.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-8.jpg'
+        BiaoTi: '三十余家资方坐镇，聚焦“明星”项目（投融界第46期创刻SHOW成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-04-04',
+        src: 'assets/imgs/offline-financing/chuangke-2.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-8.jpg'
+        BiaoTi: '医疗健康黄金时代，创投大咖齐聚这里（投融界“创刻SHOW”医疗类专场项目路演成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2018-11-28',
+        src: 'assets/imgs/offline-financing/chuangke-3.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '打动投资人，创造一刻钟的精彩！（投融界创刻SHOW第44期成功举办）',
+        DiDian: '北京',
+        RiQi: '2018-11-09',
+        src: 'assets/imgs/offline-financing/chuangke-4.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '创业时代，融资梦想等你来实现！（“创刻SHOW|双创梦想秀”专场项目路演成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2018-10-24',
+        src: 'assets/imgs/offline-financing/chuangke-5.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
-      },
-      {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-7.jpg'
+        BiaoTi: '15分钟打动投资人，好项目频出（投融界创刻SHOW第42期路演成功举办）',
+        DiDian: '北京',
+        RiQi: '2018-09-04',
+        src: 'assets/imgs/offline-financing/chuangke-6.jpg'
       }
     ]
   },
@@ -304,58 +232,58 @@ export const DataOfflineFinancingHandler = {
     enTitle: 'PAST EVENT HIGHLIGHTS',
     data: [
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '携手机构投资人，为创业者“赋能”（投融界第201期融资微路演成功举办）',
+        DiDian: '北京',
+        RiQi: '2019-05-31',
+        src: 'assets/imgs/offline-financing/weiluyan-1.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '微路演第200期“战场”，创投双方你来我往（投融界融资微路演第200期成功举办）',
+        DiDian: '上海',
+        RiQi: '2019-05-24',
+        src: 'assets/imgs/offline-financing/weiluyan-2.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '资本“大咖”聚上海，考察优质项目（投融界第409期融资沙龙成功举办）',
+        DiDian: '上海',
+        RiQi: '2019-06-05',
+        src: 'assets/imgs/offline-financing/shalong-1.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '赤子之心，乘风破浪为融资而来（投融界第408期融资沙龙成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-05-30',
+        src: 'assets/imgs/offline-financing/shalong-2.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '双创活动周来临，资本相亲汇走起（投融界第23期资本相亲汇成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-06-04',
+        src: 'assets/imgs/offline-financing/xiangqinhui-1.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '零距离面谈投资人，为创业赋能（投融界第22期资本相亲汇成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-05-29',
+        src: 'assets/imgs/offline-financing/xiangqinhui-2.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '科技自立，AI专场路演气氛火热（投融界第47期创刻SHOW成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-05-22',
+        src: 'assets/imgs/offline-financing/chuangke-1.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '三十余家资方坐镇，聚焦“明星”项目（投融界第46期创刻SHOW成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2019-04-04',
+        src: 'assets/imgs/offline-financing/chuangke-2.jpg'
       },
       {
-        BiaoTi: '我是标题',
-        DiDian: '广东 - 汕头',
-        RiQi: '2019-05-27',
-        src: 'assets/imgs/offline-financing/rongzi-6.jpg'
+        BiaoTi: '医疗健康黄金时代，创投大咖齐聚这里（投融界“创刻SHOW”医疗类专场项目路演成功举办）',
+        DiDian: '浙江 - 杭州',
+        RiQi: '2018-11-28',
+        src: 'assets/imgs/offline-financing/chuangke-3.jpg'
       }
     ]
   }
