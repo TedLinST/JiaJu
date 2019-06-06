501ea98b990aabb0b51cd483aba1e81161781a13
HPDaniel <1028854519@qq.com>
2019-06-06 15:46:22 +0800

    资金信息详细2

--- src/data/is/finding-funds.ts
+++ src/data/is/finding-funds.ts
@@ -152,7 +152,7 @@ export const DataFindingFundsHandler = {
       ShouCangShu: 18,
       YiTouZiXiangMu: 1,
       LiuLanShu: 50,
-      YaoQiuGaiShu: '...',
+      YaoQiuGaiShu: '&emsp;&emsp;本人有个人资金100万元，主要投资人工智能，机器人，智能设备，高端设备制造，高科技等项目，投资地区主要是广东地区，投资阶段不限，投资方式：股权，无前期费用，希望有意向者与我联系。',
       TouZiAnLie: '...',
       QiTaBeiZhu: '...'
     },
@@ -169,9 +169,10 @@ export const DataFindingFundsHandler = {
       ShouCangShu: 152,
       YiTouZiXiangMu: 22,
       LiuLanShu: 658,
-      YaoQiuGaiShu: '  我们致力于寻找和投资有潜力的初创期的企业和企业家，为初创期和成长期的创业团队提供股权投资、技术支持、团队构建、市场开拓、战略规划等全方位的帮助。我们主要针对高新技术行业进行投资，关注的领域有移动互联网、IT服务、医疗健康、新材料、旅游等。我们专注于天使期、早期的创业投资，每笔投资以50万-200万人民币为主。\n　如果您的团队对自己的创意和构想充满信心，如果您正在寻求独具慧眼的投资人，如果您需要一定规模、经验丰富的企业支持，欢迎您与我们联系！',
-      TouZiAnLie: '...',
-      QiTaBeiZhu: '资金介绍:金沙江创业投资基金专注于投资高新技术初创企业。我们力求发掘最杰出的创业者，共同成就伟大且具有颠覆性的高科技企业。我们致力于寻找具有优秀的团队、庞大的市场机会、核心的自主知识产权和技术，并能成为行业巨头的投资项目。去哪儿、上海大智慧和小红书等优秀创业公司最早期的机构投资者。我们深度参与并见证了他们迈向独角兽的每一步。未来，我们愿与更多优秀的创业者携手，用我们丰富的行业知识和资源，合力打造世界级的高科技企业。我们是滴滴出行、饿了么、映客直播、ofo共享单车、金沙江创业投资基金目前管理着约20亿美元的资产，包括美元和人民币基金。我们在硅谷、北京和新加坡均设立了办事处。'
+      YaoQiuGaiShu: '&emsp;&emsp;我们致力于寻找和投资有潜力的初创期的企业和企业家，为初创期和成长期的创业团队提供股权投资、技术支持、团队构建、市场开拓、战略规划等全方位的帮助。<br>&emsp;&emsp;我们主要针对高新技术行业进行投资，关注的领域有移动互联网、IT服务、医疗健康、新材料、旅游等。我们专注于天使期、早期的创业投资，每笔投资以50万-200万人民币为主。<br>&emsp;&emsp;如果您的团队对自己的创意和构想充满信心，如果您正在寻求独具慧眼的投资人，如果您需要一定规模、经验丰富的企业支持，欢迎您与我们联系！',
+      TouZiAnLie: '<img src="assets/imgs/fund-information/touzianli-2.jpg">',
+      QiTaBeiZhu: '资金介绍:<br>&emsp;&emsp;金沙江创业投资基金专注于投资高新技术初创企业。<br>&emsp;&emsp;我们力求发掘最杰出的创业者，共同成就伟大且具有颠覆性的高科技企业。我们致力于寻找具有优秀的团队、庞大的市场机会、核心的自主知识产权和技术，并能成为行业巨头的投资项目。<br>&emsp;&emsp;我们是滴滴出行、饿了么、映客直播、ofo共享单车、 去哪儿、上海大智慧和小红书等优秀创业公司最早期的机构投资者。我们深度参与并见证了他们迈向独角兽的每一步。未来，我们愿与更多优秀的创业者携手，用我们丰富的行业知识和资源，合力打造世界级的高科技企业。<br>&emsp;&emsp;金沙江创业投资基金目前管理着约20亿美元的资产，包括美元和人民币基金。我们在硅谷、北京和新加坡均设立了办事处。'
+      
     },
     {
       XiangMuBiaoTi: '红杉资本寻国内创业型家具材料研发团队',
@@ -185,7 +186,7 @@ export const DataFindingFundsHandler = {
       TouDiShu: 18,
       ShouCangShu: 233,
       YiTouZiXiangMu: 652,
-      LiuLanShu: 1,
+      LiuLanShu: 985,
       YaoQiuGaiShu: '...',
       TouZiAnLie: '...',
       QiTaBeiZhu: '...'
@@ -202,10 +203,10 @@ export const DataFindingFundsHandler = {
       TouDiShu: 0,
       ShouCangShu: 5,
       YiTouZiXiangMu: 0,
-      LiuLanShu: 1,
-      YaoQiuGaiShu: '...',
+      LiuLanShu: 20,
+      YaoQiuGaiShu: '&emsp;&emsp;个人自有资金50万元，主要想找家具经销商合作伙伴。以股权的方式，无任何前期费用，有意者请跟我联系。',
       TouZiAnLie: '...',
-      QiTaBeiZhu: '...'
+      QiTaBeiZhu: ''
     }
   ]
 }
