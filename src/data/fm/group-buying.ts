cdcf9a3a7480093f03a9e4c88c9643c873963224
HPDaniel <1028854519@qq.com>
2019-06-11 18:35:46 +0800

    家具商城 - 团购1

--- src/data/fm/group-buying.ts
+++ src/data/fm/group-buying.ts
@@ -4,20 +4,72 @@ export const DataGroupBuyingHandler = {
   TYPE_FILTER: [
     {
       id: '1',
-      title: '条件一',
+      title: '产品品类',
       data: [
         {
           id: '1',
-          label: '条件1.1'
+          label: '沙发'
         },
         {
           id: '2',
-          label: '条件1.2'
+          label: '茶几'
         },
         {
           id: '3',
-          label: '条件1.3'
-        }
+          label: '电视柜'
+        },
+        {
+          id:'4',
+          label:'角几'
+        },
+        {
+          id:'5',
+          label:'厅柜'
+        },
+        {
+          id:'6',
+          label:'床'
+        },
+        {
+          id:'7',
+          label:'床头柜'
+        },
+        {
+          id:'8',
+          label:'衣柜'
+        },
+        {
+          id:'9',
+          label:'餐桌'
+        },
+        {
+          id:'10',
+          label:'餐椅'
+        },
+        {
+          id:'11',
+          label:'边柜'
+        },
+        {
+          id:'12',
+          label:'书桌'
+        },
+        {
+          id:'13',
+          label:'书椅'
+        },
+        {
+          id:'14',
+          label:'书柜'
+        },
+        {
+          id:'15',
+          label:'休闲桌椅'
+        },
+        {
+          id:'16',
+          label:'儿童产品'
+        },
       ]
     }
   ],
@@ -25,181 +77,493 @@ export const DataGroupBuyingHandler = {
   // 列表数据
   LIST_DATA: [
     {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
+      ChanPinBiaoTi: '林氏木业北欧简约现代沙发床小户型布艺沙发三人客厅',
+      ChanPinPinLei: '沙发',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
       JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
+      JiaGe: '￥ 2150',
       YiBaoMingShuLiang: '888',
       ZongShuLiang: '1000',
       src: 'assets/imgs/group-buying/tuangou-1.jpg'
     },
     {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
+      ChanPinBiaoTi: '林氏木业北欧皮艺沙发组合客厅头层牛皮质现代真皮沙发整装RBE1K',
+      ChanPinPinLei: '沙发',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
       JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
+      JiaGe: '￥ 3990',
       YiBaoMingShuLiang: '888',
       ZongShuLiang: '1000',
       src: 'assets/imgs/group-buying/tuangou-2.jpg'
     },
     {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
+      ChanPinBiaoTi: '林氏木业现代简约客厅布艺沙发组合时尚小户型北欧家具套装',
+      ChanPinPinLei: '沙发',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
       JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
+      JiaGe: '￥ 3540',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业真皮沙发小户型客厅三人奢华头层牛皮欧式沙发组合',
+      ChanPinPinLei: '沙发',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 5980',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-1.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业北欧风格布艺沙发现代简约大小户型客厅组合家具套装',
+      ChanPinPinLei: '沙发',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 2640',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-2.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业北欧简约现代沙发床小户型布艺沙发三人客厅整装家具',
+      ChanPinPinLei: '沙发',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 999',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业简约现代真皮沙发组合欧式客厅整装头层牛皮沙发家具',
+      ChanPinPinLei: '沙发',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 4999',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-1.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业欧式布艺沙发皮沙发客厅整装小奢华家具',
+      ChanPinPinLei: '沙发',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 3789',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-2.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业欧式布艺沙发皮沙发客厅整装小奢华家具',
+      ChanPinPinLei: '沙发',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 2599',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业欧式布艺沙发皮沙发客厅整装小奢华家具',
+      ChanPinPinLei: '沙发',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 4469',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-1.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业北欧双人床简约经济型1.8米硬板床次卧实木框床家具',
+      ChanPinPinLei: '床',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 3339',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-2.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业美式乡村床1.5米1.8m木床双人床',
+      ChanPinPinLei: '床',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 2199',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-2.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业公主床1.8米双人床现代简约主卧欧式大床婚床家',
+      ChanPinPinLei: '床',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 3799',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业床现代简约时尚轻奢真皮床主卧婚床双人床',
+      ChanPinPinLei: '床',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥  2980',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-1.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业北欧风格床简约现代1.8米实木脚布艺双人床',
+      ChanPinPinLei: '床',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 2399',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-2.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业1.8米头层牛皮床真皮床简约现代软床主卧双人床',
+      ChanPinPinLei: '床',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 2399',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业美式乡村床1.5米1.8m木床双人床',
+      ChanPinPinLei: '床',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 3199',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-2.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业1.2米女孩床',
+      ChanPinPinLei: '床',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 1999',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '儿童床女孩公主床单人床',
+      ChanPinPinLei: '床',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 1760',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-2.jpg'
+    },
+    {
+      ChanPinBiaoTi: '林氏木业女孩床公主床少女床欧式1.5米',
+      ChanPinPinLei: '床',
+      PinPai: '林氏木业',
+      DianPu: '林氏木业家具旗舰店',
+      ChanDi: '广东佛山',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 2468',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: 'ins北欧实木电视柜茶几套装组合小户型日式家具简约现代客厅机柜',
+      ChanPinPinLei: '电视柜',
+      PinPai: '段式木业',
+      DianPu: '月烨家居旗舰店',
+      ChanDi: '江苏徐州',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 398',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: 'ins北欧实木电视柜茶几套装组合小户型日式家具简约现代客厅机柜',
+      ChanPinPinLei: '电视柜',
+      PinPai: '段式木业',
+      DianPu: '月烨家居旗舰店',
+      ChanDi: '江苏徐州',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 308',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: 'ins北欧实木简约现代小户型客厅卧室电视柜日式家具组合玻璃移门',
+      ChanPinPinLei: '电视柜',
+      PinPai: '段式木业',
+      DianPu: '月烨家居旗舰店',
+      ChanDi: '江苏徐州',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 238',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '北欧实木茶几钢化玻璃易简约时尚现代创意小户型茶几客厅组合方型',
+      ChanPinPinLei: '茶几',
+      PinPai: '段式木业',
+      DianPu: '月烨家居旗舰店',
+      ChanDi: '江苏徐州',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 250',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '茶几简约现代钢化玻璃客厅个性家具组合创意小户型办公室方形桌子',
+      ChanPinPinLei: '茶几',
+      PinPai: '段式木业',
+      DianPu: '月烨家居旗舰店',
+      ChanDi: '江苏徐州',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 150',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '月烨家居茶几简约现代钢化玻璃茶几 客厅简易小户型创意茶几桌子',
+      ChanPinPinLei: '茶几',
+      PinPai: '段式木业',
+      DianPu: '月烨家居旗舰店',
+      ChanDi: '江苏徐州',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 158',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: 'ins风实木简约北欧日式钢化玻璃茶几小户型矮桌子创意易客厅现代',
+      ChanPinPinLei: '茶几',
+      PinPai: '段式木业',
+      DianPu: '月烨家居旗舰店',
+      ChanDi: '江苏徐州',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 268',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: 'ins风实木简约北欧茶几小户型矮桌子创意咖啡桌易装客厅现代边几',
+      ChanPinPinLei: '茶几',
+      PinPai: '段式木业',
+      DianPu: '月烨家居旗舰店',
+      ChanDi: '江苏徐州',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 148',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '茶几简约现代钢化玻璃客厅个性家具组合创意小户型',
+      ChanPinPinLei: '茶几',
+      PinPai: '段式木业',
+      DianPu: '月烨家居旗舰店',
+      ChanDi: '江苏徐州',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 118',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '月烨家居茶几简约现代钢化玻璃茶几',
+      ChanPinPinLei: '茶几',
+      PinPai: '段式木业',
+      DianPu: '月烨家居旗舰店',
+      ChanDi: '江苏徐州',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 158',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '源氏木语实木床头柜北欧经济型床头收纳柜现代简约卧室简易小柜子',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 400',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '源氏木语实木小三抽床头柜白橡木床边柜卧室简约现代小柜子储物柜',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 740',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '源氏木语纯实木床头柜白橡木单抽灯桌柜北欧小斗橱环保储物柜卧室',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 580',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '源氏木语全实木床头柜北欧橡木收纳储物柜现代简约卧室床边小柜子',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 680',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '源氏木语实木床头柜北欧卧室床边小柜子现代简约经济型简易储物柜',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 550',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '源氏木语纯实木床头柜白橡木两抽柜北欧简约小斗橱环保储物柜卧室',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 830',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '源氏木语纯实木储物床头柜北欧带锁收纳柜简约斗柜床边桌卧室家具',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 1080',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '源氏木语实木双抽床头柜白橡木迷你床边柜北欧简约现代卧室收纳柜',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 740',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '源氏木语全实木床头柜北欧橡木儿童储物柜现代简约卧室床边小柜子',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 680',
+      YiBaoMingShuLiang: '888',
+      ZongShuLiang: '1000',
+      src: 'assets/imgs/group-buying/tuangou-3.jpg'
+    },
+    {
+      ChanPinBiaoTi: '源氏木语纯实木床头柜白橡木木蜡油北欧抽屉收纳三斗橱卧室床边桌',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
+      JieZhiShiJian: '2019-05-29',
+      JiaGe: '￥ 1080',
       YiBaoMingShuLiang: '888',
       ZongShuLiang: '1000',
       src: 'assets/imgs/group-buying/tuangou-3.jpg'
     },
     {
       ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
+      ChanPinPinLei: '床头柜',
+      PinPai: '源氏木语',
+      DianPu: '源氏木语家居旗舰店',
+      ChanDi: '上海',
       JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-1.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-2.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-3.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-1.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-2.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-3.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-1.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-2.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-3.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-1.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
-      YiBaoMingShuLiang: '888',
-      ZongShuLiang: '1000',
-      src: 'assets/imgs/group-buying/tuangou-2.jpg'
-    },
-    {
-      ChanPinBiaoTi: 'XXXX',
-      ChanPinPinLei: 'XXXX',
-      PinPai: 'XXX',
-      DianPu: 'XXX',
-      ChanDi: 'XXX',
-      JieZhiShiJian: '2019-05-29',
-      JiaGe: '￥8000',
+      JiaGe: '￥ 8000',
       YiBaoMingShuLiang: '888',
       ZongShuLiang: '1000',
       src: 'assets/imgs/group-buying/tuangou-3.jpg'
