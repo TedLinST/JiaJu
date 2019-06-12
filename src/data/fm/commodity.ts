492f7011891ed0fd279eca8e27473bd5d69008fa
HPDaniel <1028854519@qq.com>
2019-06-12 15:32:40 +0800

    家具商城 - 商品-条件选择区

--- src/data/fm/commodity.ts
+++ src/data/fm/commodity.ts
@@ -4,20 +4,72 @@ export const DataCommodityHandler = {
   TYPE_FILTER: [
     {
       id: '1',
-      title: '条件一',
+      title: '产品品类',
       data: [
         {
           id: '1',
-          label: '条件1.1'
+          label: '沙发'
         },
         {
           id: '2',
-          label: '条件1.2'
+          label: '茶几'
         },
         {
           id: '3',
-          label: '条件1.3'
-        }
+          label: '电视柜'
+        },
+        {
+          id:'4',
+          label:'角几'
+        },
+        {
+          id:'5',
+          label:'厅柜'
+        },
+        {
+          id:'6',
+          label:'床'
+        },
+        {
+          id:'7',
+          label:'床头柜'
+        },
+        {
+          id:'8',
+          label:'衣柜'
+        },
+        {
+          id:'9',
+          label:'餐桌'
+        },
+        {
+          id:'10',
+          label:'餐椅'
+        },
+        {
+          id:'11',
+          label:'边柜'
+        },
+        {
+          id:'12',
+          label:'书桌'
+        },
+        {
+          id:'13',
+          label:'书椅'
+        },
+        {
+          id:'14',
+          label:'书柜'
+        },
+        {
+          id:'15',
+          label:'休闲桌椅'
+        },
+        {
+          id:'16',
+          label:'儿童产品'
+        },
       ]
     }
   ]
