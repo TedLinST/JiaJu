bb5192a9d728cee9b653ccddc039f7013a3f8c35
HPDaniel <1028854519@qq.com>
2019-06-14 18:10:41 +0800

    家具商城-品牌

--- src/data/fm/brand.ts
+++ src/data/fm/brand.ts
@@ -4,146 +4,291 @@ export const DataBrandHandler = {
   BR: {
     title: '品牌推荐',
     enTitle: 'BRAND RECOMMENDATION',
-    // 主营业务
+    // 主营业务 - 实木家具
     data0: [
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '赖氏',
+        src: 'assets/imgs/brand/01SMpinpai-1.jpg'
       },
       {
-        title: 'Logo2',
-        src: 'assets/imgs/brand/pinpai-2.jpg'
+        title: '南洋迪克',
+        src: 'assets/imgs/brand/01SMpinpai-2.jpg'
       },
       {
-        title: 'Logo3',
-        src: 'assets/imgs/brand/pinpai-3.jpg'
+        title: '宜华生活',
+        src: 'assets/imgs/brand/01SMpinpai-3.jpg'
       },
       {
-        title: 'Logo4',
-        src: 'assets/imgs/brand/pinpai-4.jpg'
+        title: '柏森',
+        src: 'assets/imgs/brand/01SMpinpai-4.jpg'
       },
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '百强',
+        src: 'assets/imgs/brand/01SMpinpai-5.jpg'
       },
       {
-        title: 'Logo2',
-        src: 'assets/imgs/brand/pinpai-2.jpg'
+        title: '华日',
+        src: 'assets/imgs/brand/01SMpinpai-6.jpg'
       },
       {
-        title: 'Logo3',
-        src: 'assets/imgs/brand/pinpai-3.jpg'
+        title: '苏梨',
+        src: 'assets/imgs/brand/01SMpinpai-7.jpg'
       },
       {
-        title: 'Logo4',
-        src: 'assets/imgs/brand/pinpai-4.jpg'
+        title: '阅梨',
+        src: 'assets/imgs/brand/01SMpinpai-8.jpg'
       }
     ],
-    // 主营风格
+    // 主营业务 - 板式家具
+    // data0: [
+    //   {
+    //     title: '双虎',
+    //     src: 'assets/imgs/brand/01BSpinpai-1.jpg'
+    //   },
+    //   {
+    //     title: '好莱客',
+    //     src: 'assets/imgs/brand/01BSpinpai-2.jpg'
+    //   },
+    //   {
+    //     title: '意风',
+    //     src: 'assets/imgs/brand/01BSpinpai-3.jpg'
+    //   },
+    //   {
+    //     title: 'KD',
+    //     src: 'assets/imgs/brand/01BSpinpai-4.jpg'
+    //   },
+    //   {
+    //     title: '楷模',
+    //     src: 'assets/imgs/brand/01BSpinpai-5.jpg'
+    //   },
+    //   {
+    //     title: '雅梦娜-RH',
+    //     src: 'assets/imgs/brand/01BSpinpai-6.jpg'
+    //   },
+    //   {
+    //     title: '多维尚书',
+    //     src: 'assets/imgs/brand/01BSpinpai-7.jpg'
+    //   },
+    //   {
+    //     title: '永和轩',
+    //     src: 'assets/imgs/brand/01BSpinpai-8.jpg'
+    //   },
+    // ],
+    
+    // 主营风格 - 现代中式
     data1: [
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '华丰',
+        src: 'assets/imgs/brand/02XDpinpai-1.jpg'
       },
       {
-        title: 'Logo2',
-        src: 'assets/imgs/brand/pinpai-2.jpg'
+        title: '光明',
+        src: 'assets/imgs/brand/02XDpinpai-2.jpg'
       },
       {
-        title: 'Logo3',
-        src: 'assets/imgs/brand/pinpai-3.jpg'
+        title: '双叶',
+        src: 'assets/imgs/brand/02XDpinpai-3.jpg'
       },
       {
-        title: 'Logo4',
-        src: 'assets/imgs/brand/pinpai-4.jpg'
+        title: '南洋胡氏',
+        src: 'assets/imgs/brand/02XDpinpai-4.jpg'
       },
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '大红鹰',
+        src: 'assets/imgs/brand/02XDpinpai-5.jpg'
       },
       {
-        title: 'Logo2',
-        src: 'assets/imgs/brand/pinpai-2.jpg'
+        title: '联邦',
+        src: 'assets/imgs/brand/02XDpinpai-6.jpg'
       },
       {
-        title: 'Logo3',
-        src: 'assets/imgs/brand/pinpai-3.jpg'
+        title: '华鹤',
+        src: 'assets/imgs/brand/02XDpinpai-7.jpg'
       },
       {
-        title: 'Logo4',
-        src: 'assets/imgs/brand/pinpai-4.jpg'
+        title: '和风',
+        src: 'assets/imgs/brand/02XDpinpai-8.jpg'
       },
     ],
-    // 主营地区
+    // 主营风格 - 新中式
+    // data1: [
+    //   {
+    //     title: '华颂',
+    //     src: 'assets/imgs/brand/02XZpinpai-1.jpg'
+    //   },
+    //   {
+    //     title: '御福堂',
+    //     src: 'assets/imgs/brand/02XZpinpai-2.jpg'
+    //   },
+    //   {
+    //     title: '江南别院',
+    //     src: 'assets/imgs/brand/02XZpinpai-3.jpg'
+    //   },
+    //   {
+    //     title: '京瓷',
+    //     src: 'assets/imgs/brand/02XZpinpai-4.jpg'
+    //   },
+    //   {
+    //     title: '东方荟',
+    //     src: 'assets/imgs/brand/02XZpinpai-5.jpg'
+    //   },
+    //   {
+    //     title: '东家',
+    //     src: 'assets/imgs/brand/02XZpinpai-6.jpg'
+    //   },
+    //   {
+    //     title: '自在工坊',
+    //     src: 'assets/imgs/brand/02XZpinpai-7.jpg'
+    //   },
+    //   {
+    //     title: '多少',
+    //     src: 'assets/imgs/brand/02XZpinpai-8.jpg'
+    //   },
+    // ],
+
+    // 主营地区 - 华南
     data2: [
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '华睿',
+        src: 'assets/imgs/brand/03HNpinpai-1.jpg'
       },
       {
-        title: 'Logo2',
-        src: 'assets/imgs/brand/pinpai-2.jpg'
+        title: '众工新故式',
+        src: 'assets/imgs/brand/03HNpinpai-2.jpg'
       },
       {
-        title: 'Logo3',
-        src: 'assets/imgs/brand/pinpai-3.jpg'
+        title: '慕木',
+        src: 'assets/imgs/brand/03HNpinpai-3.jpg'
       },
       {
-        title: 'Logo4',
-        src: 'assets/imgs/brand/pinpai-4.jpg'
+        title: '平仄',
+        src: 'assets/imgs/brand/03HNpinpai-4.jpg'
       },
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '典木轩',
+        src: 'assets/imgs/brand/03HNpinpai-5.jpg'
       },
       {
-        title: 'Logo2',
-        src: 'assets/imgs/brand/pinpai-2.jpg'
+        title: '明堂',
+        src: 'assets/imgs/brand/03HNpinpai-6.jpg'
       },
       {
-        title: 'Logo3',
-        src: 'assets/imgs/brand/pinpai-3.jpg'
+        title: '圣木雅',
+        src: 'assets/imgs/brand/03HNpinpai-7.jpg'
       },
       {
-        title: 'Logo4',
-        src: 'assets/imgs/brand/pinpai-4.jpg'
+        title: '诺佳',
+        src: 'assets/imgs/brand/03HNpinpai-8.jpg'
       }
     ],
-    // 产品来源
+
+    // 主营地区 - 华东
+    // data2: [
+    //   {
+    //     title: '澳凡',
+    //     src: 'assets/imgs/brand/03HDpinpai-1.jpg'
+    //   },
+    //   {
+    //     title: '优品U舍',
+    //     src: 'assets/imgs/brand/03HDpinpai-2.jpg'
+    //   },
+    //   {
+    //     title: '优家',
+    //     src: 'assets/imgs/brand/03HDpinpai-3.jpg'
+    //   },
+    //   {
+    //     title: '喜临门',
+    //     src: 'assets/imgs/brand/03HDpinpai-4.jpg'
+    //   },
+    //   {
+    //     title: '如迪',
+    //     src: 'assets/imgs/brand/03HDpinpai-5.jpg'
+    //   },
+    //   {
+    //     title: '福邸',
+    //     src: 'assets/imgs/brand/03HDpinpai-6.jpg'
+    //   },
+    //   {
+    //     title: '斯可馨',
+    //     src: 'assets/imgs/brand/03HDpinpai-7.jpg'
+    //   },
+    //   {
+    //     title: '元鸿',
+    //     src: 'assets/imgs/brand/03HDpinpai-8.jpg'
+    //   }
+    // ],
+
+    // 产品来源 - 国产
     data3: [
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '元宗',
+        src: 'assets/imgs/brand/04GCpinpai-1.jpg'
       },
       {
-        title: 'Logo2',
-        src: 'assets/imgs/brand/pinpai-2.jpg'
+        title: '戴为红木',
+        src: 'assets/imgs/brand/04GCpinpai-2.jpg'
       },
       {
-        title: 'Logo3',
-        src: 'assets/imgs/brand/pinpai-3.jpg'
+        title: '滑氏红木',
+        src: 'assets/imgs/brand/04GCpinpai-3.jpg'
       },
       {
-        title: 'Logo4',
-        src: 'assets/imgs/brand/pinpai-4.jpg'
+        title: '维莎原木',
+        src: 'assets/imgs/brand/04GCpinpai-4.jpg'
       },
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '源氏木语',
+        src: 'assets/imgs/brand/04GCpinpai-5.jpg'
       },
       {
-        title: 'Logo2',
-        src: 'assets/imgs/brand/pinpai-2.jpg'
+        title: '青岛一木',
+        src: 'assets/imgs/brand/04GCpinpai-6.jpg'
       },
       {
-        title: 'Logo3',
-        src: 'assets/imgs/brand/pinpai-3.jpg'
+        title: '北欧E家',
+        src: 'assets/imgs/brand/04GCpinpai-7.jpg'
       },
       {
-        title: 'Logo4',
-        src: 'assets/imgs/brand/pinpai-4.jpg'
+        title: '优梵艺术',
+        src: 'assets/imgs/brand/04GCpinpai-8.jpg'
       }
     ]
+
+    // 产品来源 - 进口
+    // data3: [
+    //   {
+    //     title: '纳图兹',
+    //     src: 'assets/imgs/brand/04JKpinpai-1.jpg'
+    //   },
+    //   {
+    //     title: '爱室丽',
+    //     src: 'assets/imgs/brand/04JKpinpai-2.jpg'
+    //   },
+    //   {
+    //     title: 'Harbor House',
+    //     src: 'assets/imgs/brand/04JKpinpai-3.jpg'
+    //   },
+    //   {
+    //     title: '瑞吉欧',
+    //     src: 'assets/imgs/brand/04JKpinpai-4.jpg'
+    //   },
+    //   {
+    //     title: 'Visionnaire',
+    //     src: 'assets/imgs/brand/04JKpinpai-5.jpg'
+    //   },
+    //   {
+    //     title: 'Fratelli Radice',
+    //     src: 'assets/imgs/brand/04JKpinpai-6.jpg'
+    //   },
+    //   {
+    //     title: 'PAIDI',
+    //     src: 'assets/imgs/brand/04JKpinpai-7.jpg'
+    //   },
+    //   {
+    //     title: 'Halley',
+    //     src: 'assets/imgs/brand/04JKpinpai-8.jpg'
+    //   }
+    // ]
   },
 
   // 精选品牌
@@ -152,53 +297,77 @@ export const DataBrandHandler = {
     enTitle: 'FEATURED BRAND',
     data: [
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '索菲亚',
+        src: 'assets/imgs/brand/JXpinpai-1.jpg'
       },
       {
-        title: 'Logo2',
+        title: '尚品宅配',
         src: 'assets/imgs/brand/pinpai-2.jpg'
       },
       {
-        title: 'Logo3',
+        title: '林氏木业',
         src: 'assets/imgs/brand/pinpai-3.jpg'
       },
       {
-        title: 'Logo4',
+        title: '欧派',
         src: 'assets/imgs/brand/pinpai-4.jpg'
       },
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '曲美家居',
+        src: 'assets/imgs/brand/pinpai-5.jpg'
       },
       {
-        title: 'Logo2',
-        src: 'assets/imgs/brand/pinpai-2.jpg'
+        title: '全友家居',
+        src: 'assets/imgs/brand/pinpai-6.jpg'
       },
       {
-        title: 'Logo3',
-        src: 'assets/imgs/brand/pinpai-3.jpg'
+        title: '美克美家',
+        src: 'assets/imgs/brand/pinpai-7.jpg'
       },
       {
-        title: 'Logo4',
-        src: 'assets/imgs/brand/pinpai-4.jpg'
+        title: '红苹果家具',
+        src: 'assets/imgs/brand/pinpai-8.jpg'
       },
       {
-        title: 'Logo1',
-        src: 'assets/imgs/brand/pinpai-1.jpg'
+        title: '顾家家居',
+        src: 'assets/imgs/brand/pinpai-9.jpg'
       },
       {
-        title: 'Logo2',
-        src: 'assets/imgs/brand/pinpai-2.jpg'
+        title: '仁豪家居',
+        src: 'assets/imgs/brand/pinpai-10.jpg'
       },
       {
-        title: 'Logo3',
-        src: 'assets/imgs/brand/pinpai-3.jpg'
+        title: '童话森林',
+        src: 'assets/imgs/brand/pinpai-11.jpg'
       },
       {
-        title: 'Logo4',
-        src: 'assets/imgs/brand/pinpai-4.jpg'
-      }
+        title: '长江家具',
+        src: 'assets/imgs/brand/pinpai-12.jpg'
+      },
+      {
+        title: '左右沙发',
+        src: 'assets/imgs/brand/pinpai-13.jpg'
+      },
+      {
+        title: '爱依瑞斯',
+        src: 'assets/imgs/brand/pinpai-14.jpg'
+      },
+      {
+        title: '南洋胡氏',
+        src: 'assets/imgs/brand/pinpai-15.jpg'
+      },
+      {
+        title: '皇朝家私',
+        src: 'assets/imgs/brand/pinpai-16.jpg'
+      },
+      {
+        title: '华日家具',
+        src: 'assets/imgs/brand/pinpai-17.jpg'
+      },
+      {
+        title: '博洛尼',
+        src: 'assets/imgs/brand/pinpai-18.jpg'
+      },
     ]
   }
 }
