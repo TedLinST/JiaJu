8c4ad0165a4f964ce9af6c4f10a6f49728c75d67
TedLinST <Ted-Lin@qq.com>
2019-07-04 18:12:14 +0800

    查漏补缺-业务系统

--- src/data/er/prod/location-information.ts
+++ src/data/er/prod/location-information.ts
@@ -7,91 +7,91 @@ export const DataProdLocationInformationHandler = {
       KuanShi: '欧式衣柜',
       subData: [
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "柜架",
           BuJianMingCheng: "柜架总成"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "2",
           BeiZhu: "",
           BuWei: "侧板",
           BuJianMingCheng: "侧板组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "柜架",
           BuJianMingCheng: "柜架总成"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "柜架",
           BuJianMingCheng: "前框组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "底盘",
           BuJianMingCheng: "底盘组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "2",
           BeiZhu: "",
           BuWei: "抽屉",
           BuJianMingCheng: "上抽屉框"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "2",
           BeiZhu: "",
           BuWei: "抽屉",
           BuJianMingCheng: "中下抽屉框"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "2",
           BeiZhu: "",
           BuWei: "柜架",
           BuJianMingCheng: "面板前加厚条"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "2",
           BeiZhu: "",
           BuWei: "柜架",
           BuJianMingCheng: "侧板"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "柜架",
           BuJianMingCheng: "前内立档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "柜架",
           BuJianMingCheng: "后立档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "2",
           BeiZhu: "",
           BuWei: "抽屉",
           BuJianMingCheng: "路轨条"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "2",
           BeiZhu: "",
           BuWei: "抽屉",
           BuJianMingCheng: "加固条"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "柜架",
           BuJianMingCheng: "面板总成"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "柜架",
           BuJianMingCheng: "后上横档"
@@ -104,25 +104,25 @@ export const DataProdLocationInformationHandler = {
       KuanShi: '欧式餐椅',
       subData: [
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "总成",
           BuJianMingCheng: "餐椅总成"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "椅架",
           BuJianMingCheng: "椅架总成"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "4",
           BeiZhu: "",
           BuWei: "椅架",
           BuJianMingCheng: "支撑脚"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "2",
           BeiZhu: "",
           BuWei: "椅架",
           BuJianMingCheng: "护栏托条"
@@ -136,121 +136,121 @@ export const DataProdLocationInformationHandler = {
       KuanShi: '加州床',
       subData: [
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头总成"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾板"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头板组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "4",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头上段脚"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头上横档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾板组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "4",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头下段脚"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾左右立档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾上横档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾脚"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾脚基板"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "护栏",
           BuJianMingCheng: "护栏组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "护栏",
           BuJianMingCheng: "护栏面板组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾上压条"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾下横档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头板"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "软包内上横条"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头上压条"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头肚板"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头片榫"
@@ -264,121 +264,121 @@ export const DataProdLocationInformationHandler = {
       KuanShi: '皇后床',
       subData: [
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾板"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头板组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "4",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头上段脚"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头上横档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾板组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头总成"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "4",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头下段脚"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾左右立档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾上横档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾脚基板"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "护栏",
           BuJianMingCheng: "护栏组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "护栏",
           BuJianMingCheng: "护栏面板组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "4",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾脚"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾下横档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头板"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "软包内上横条"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床尾",
           BuJianMingCheng: "床尾上压条"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头上压条"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头肚板"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "床头",
           BuJianMingCheng: "床头片榫"
@@ -392,49 +392,49 @@ export const DataProdLocationInformationHandler = {
       KuanShi: '欧式餐桌',
       subData: [
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "总成",
           BuJianMingCheng: "餐桌总成"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "总成",
           BuJianMingCheng: "面板总成"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "2",
           BeiZhu: "",
           BuWei: "面板",
           BuJianMingCheng: "面板1"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "2",
           BeiZhu: "",
           BuWei: "面板",
           BuJianMingCheng: "面板2"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "面板",
           BuJianMingCheng: "面板加厚框"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "侧板",
           BuJianMingCheng: "侧板组合"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "侧板",
           BuJianMingCheng: "前内立档"
         },
         {
-          DuLiangFangFa: "计件",
+          DuLiangFangFa: "1",
           BeiZhu: "",
           BuWei: "侧板",
           BuJianMingCheng: "后立档"
